5bfc9ab48cfc7fdd9bb078e574a70545c2102a97
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-13 16:05:56 +0200

    remove repeats

--- app/main.py
+++ app/main.py
@@ -7,24 +7,10 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [
-        {"line": errors[i]["line_number"],
-         "column": errors[i]["column_number"],
-         "message": errors[i]["text"],
-         "name": errors[i]["code"],
-         "source": "flake8"}
-        for i in range(len(errors))],
-        "path": str(file_path),
-        "status": "failed" if len(errors) else "passed"}
+    return {"errors": [format_linter_error(i) for i in errors],
+            "path": str(file_path),
+            "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [
-        {"line": v[i]["line_number"],
-         "column": v[i]["column_number"],
-         "message": v[i]["text"],
-         "name": v[i]["code"],
-         "source": "flake8"}
-        for i in range(len(v))], "path": k, "status": "failed"} if len(v) != 0
-        else {"errors": v, "path": k, "status": "passed"}
-        for k, v in linter_report.items()]
+    return [format_single_linter_file(k, v) for k, v in linter_report.items()]
