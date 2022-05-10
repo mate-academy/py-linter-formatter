f4055db6418e5c266a5a9e20221f26626f4dc3ae
Taras Nester <tarasnester414@gmail.com>
2022-05-10 14:37:14 +0300

    improved format_single_linter_file if else statement

--- app/main.py
+++ app/main.py
@@ -12,15 +12,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         'errors': [format_linter_error(error) for error in errors],
         'path': file_path,
-        'status': (
-            'passed' if not [format_linter_error(error)
-                             for error in errors]
-            else 'failed'),
+        'status': ('passed' if not errors else 'failed'),
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file, errors)
-        for file, errors in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
