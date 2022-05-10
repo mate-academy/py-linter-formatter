9dfeb28fa6a1284b1740e981a91a9a99331318b7
MykolaIF <nick.pasko.82@gmail.com>
2022-05-10 23:31:17 +0300

    Comments removed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +9,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
     return {
         "errors": [format_linter_error(err) for err in errors],
         "path": file_path,
@@ -19,7 +17,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
     return [
         format_single_linter_file(key_path, errors_value)
         for key_path, errors_value in linter_report.items()
