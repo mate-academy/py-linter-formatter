1e216b15239dee753b7a855361f0f942e94c943c
Dima Svirsa <august4smile@gmail.com>
2022-05-11 13:34:33 +0300

    'Solution_flake8_better'

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "passed"
-            if [format_linter_error(error) for error in errors] == []
-            else "failed"
-            }
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed"
+        if [format_linter_error(error) for error in errors] == []
+        else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
