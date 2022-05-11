24bcf129d415e870b61d65d0cde30c913e5589c6
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-11 13:02:14 +0300

    СÑCorrect my code style

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ def format_linter_error(error: dict) -> dict:
             "column": error['column_number'],
             "message": error['text'],
             "name": error['code'],
-            "source": "flake8"
-            }
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
