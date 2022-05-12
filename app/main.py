cc4108e7c11bcb2d0b7470e60f9e844a5cc525e2
Alexandr <netiazhevskyi@gmail.com>
2022-05-12 17:21:39 +0300

    3 functions are made to properly convert report

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ def format_linter_error(error: dict) -> dict:
                 "column": error.get("column_number"),
                 "message": error.get("text"),
                 "name": error.get("code"),
-                "source": "flake8",
-            }
+                "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -18,5 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(path, error) for path, error in linter_report.items()
+        format_single_linter_file(path, error)
+        for path, error in linter_report.items()
     ]
