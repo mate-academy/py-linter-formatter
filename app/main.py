ddb05d1e6fe7821228f8a9744cbc9b4c73775433
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-12 16:10:30 +0300

    'Solution-12/05#2'

--- app/main.py
+++ app/main.py
@@ -9,23 +9,16 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
 
-    return {"errors": [{"line": errors[i]["line_number"],
-                        "column": errors[i]["column_number"],
-                        "message": errors[i]["text"],
-                        "name": errors[i]["code"],
-                        "source": "flake8"} for i in range(len(errors))],
+    return {"errors": [(format_linter_error(errors[i]))
+                       for i in range(len(errors))],
             "path": file_path,
             "status": "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
 
-    return [{"errors": [
-        {"line": linter_report[path][path_in]["line_number"],
-         "column": linter_report[path][path_in]["column_number"],
-         "message": linter_report[path][path_in]["text"],
-         "name": linter_report[path][path_in]["code"],
-         "source": "flake8"} for path_in in range(len(linter_report[path]))],
-        "path": f"{path}",
-        "status": ("passed" if linter_report[path] == [] else "failed")}
-        for path, error in linter_report.items()]
+    return [{"errors": [(format_linter_error(linter_report[path][path_in]))
+                        for path_in in range(len(linter_report[path]))],
+             "path": f"{path}",
+             "status": ("passed" if linter_report[path] == [] else "failed")}
+            for path, error in linter_report.items()]
