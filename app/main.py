67ebcb58f622fb241d70a92b470b5283b8122e67
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-05-16 12:47:23 +0300

    Two after flake8 and pytest

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    return{
+    return {
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
@@ -9,7 +9,7 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return{
+    return {
         "errors":
         [
             format_linter_error(errors[i])
@@ -21,7 +21,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return[
+    return [
         format_single_linter_file(key, value)
         for key, value in linter_report.items()
     ]
