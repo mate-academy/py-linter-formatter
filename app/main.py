0e3b487db431a10b61a4b7c65c887072c73345a4
Dima Melnyk <melnikd111@gmail.com>
2022-05-18 18:38:34 +0300

    reult without oneliner

--- app/main.py
+++ app/main.py
@@ -1,13 +1,33 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
+    res_e = {}
+    value_list = []
+    for value in error.values():
+        value_list.append(value)
+    res_e['line'] = value_list[2]
+    res_e['column'] = value_list[3]
+    res_e['message'] = value_list[4]
+    res_e['name'] = value_list[0]
+    res_e['source'] = 'flake8'
+    return res_e
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
+    res_single = []
+    single = {}
+    for error in errors:
+        res_single.append(format_linter_error(error))
+    single['errors'] = res_single
+    single['path'] = file_path
+    if not single['errors']:
+        single['status'] = 'passed'
+    return single
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+            ]
     pass
