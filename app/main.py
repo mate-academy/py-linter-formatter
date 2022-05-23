886e76c56dcab0207c11f76f3be57d0065da41c6
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-05-23 23:15:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ def format_linter_error(error: dict) -> dict:
         "column": error[key][i]["column_number"],
         "message":error[key][i]["text"],
         "name": error[key][i]["code"],
-        "source": "flake8"}if len(error[key]) else [] for i in range(len(error[key]))],
+        "source": "flake8"}if len(error[key]) else []
+        for i in range(len(error[key]))],
         "path": key,
         "status": "failed"if len(error[key]) else"passed"}for key in error][0]
 
@@ -19,7 +20,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         if len(errors[key]) else []
         for i in range(len(errors[key]))],
         "path": key,
-        "status": "failed" if len(errors[key]) else "passed"} for key in errors if file_path == key][0]
+        "status": "failed" if len(errors[key]) else "passed"}
+        for key in errors if file_path == key][0]
 
 
 def format_linter_report(linter_report: dict) -> list:
@@ -29,6 +31,8 @@ def format_linter_report(linter_report: dict) -> list:
         "message":linter_report[key][i]["text"],
         "name": linter_report[key][i]["code"],
         "source": "flake8"}
-        if len(linter_report[key]) else []for i in range(len(linter_report[key]))],
+        if len(linter_report[key]) else []
+        for i in range(len(linter_report[key]))],
         "path": key,
-        "status": "failed" if len(linter_report[key]) else "passed"}for key in linter_report]
+        "status": "failed" if len(linter_report[key]) else "passed"}
+        for key in linter_report]
