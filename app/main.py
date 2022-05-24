32612edbc10259e724817289847363f62c84ce21
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-05-24 16:35:34 +0300

    Solutin

--- app/main.py
+++ app/main.py
@@ -1,38 +1,18 @@
 def format_linter_error(error: dict) -> dict:
-    return [{"errors": [{
-        "line": error[key][i]["line_number"],
-        "column": error[key][i]["column_number"],
-        "message":error[key][i]["text"],
-        "name": error[key][i]["code"],
-        "source": "flake8"}if len(error[key]) else []
-        for i in range(len(error[key]))],
-        "path": key,
-        "status": "failed"if len(error[key]) else"passed"}for key in error][0]
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return [{"errors": [{
-        "line": errors[key][i]["line_number"],
-        "column": errors[key][i]["column_number"],
-        "message": errors[key][i]["text"],
-        "name": errors[key][i]["code"],
-        "source": "flake8"}
-        if len(errors[key]) else []
-        for i in range(len(errors[key]))],
-        "path": key,
-        "status": "failed" if len(errors[key]) else "passed"}
-        for key in errors if file_path == key][0]
+    return {"errors": [format_linter_error(error)for error in errors],
+            "path": file_path,
+            "status": "passed" if not len(errors) else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [{
-        "line": linter_report[key][i]["line_number"],
-        "column": linter_report[key][i]["column_number"],
-        "message":linter_report[key][i]["text"],
-        "name": linter_report[key][i]["code"],
-        "source": "flake8"}
-        if len(linter_report[key]) else []
-        for i in range(len(linter_report[key]))],
-        "path": key,
-        "status": "failed" if len(linter_report[key]) else "passed"}
-        for key in linter_report]
+    return [format_single_linter_file(keys, value)
+            for keys, value in linter_report.items()]
