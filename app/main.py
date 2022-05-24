141318f2e9b906cd6c153e6d84565a40f85bf3d5
Andrii Padalko <a.padalko@gmail.com>
2022-05-24 18:03:57 +0300

    DRY-Solution

--- app/main.py
+++ app/main.py
@@ -10,13 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [
-        {
-            "line": errors[i]["line_number"],
-            "column": errors[i]["column_number"],
-            "message": errors[i]["text"],
-            "name": errors[i]["code"],
-            "source": "flake8"}
-        for i in range(len(errors))
+        format_linter_error(errors[i]) for i in range(len(errors))
     ],
         "path": file_path,
         "status": "failed" if errors else "passed"
@@ -25,16 +19,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [
-            {
-                "line": linter_report[key][i]["line_number"],
-                "column": linter_report[key][i]["column_number"],
-                "message": linter_report[key][i]["text"],
-                "name": linter_report[key][i]["code"],
-                "source": "flake8"}
-            for i in range(len(linter_report[key]))
-        ],
-            "path": key,
-            "status": "failed" if linter_report[key] else "passed"
-        } for key in linter_report
+        format_single_linter_file(key, linter_report[key])
+        for key in linter_report
     ]
