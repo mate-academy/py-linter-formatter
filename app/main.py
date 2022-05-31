56050b14f76a0ab408549c088aac2247e269c1e6
SenchaPencha <SenchaPencha@mail.ru>
2022-05-31 14:00:35 +0300

    Second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error['line_number'],
-        "column": error['column_number'],
-        "message": error['text'],
-        "name": error['code'],
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8",
     }
 
@@ -11,7 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(i) for i in errors],
             "path": f"{file_path}",
-            "status": "failed" if errors != [] else 'passed'}
+            "status": "failed" if errors != [] else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
