669ae21f6dff77ac60fbeedd903456eaba7a832f
Denys Belei <denysbelei96>
2022-06-13 17:46:34 +0100

    flake 8 also fixed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -16,6 +15,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "passed" if len(errors) == 0 else "failed"
     }
 
-def format_linter_report(linter_report: dict) -> list:
 
-    return [format_single_linter_file(file_name, linter_report[file_name]) for file_name in linter_report]
+def format_linter_report(linter_report: dict) -> list:
+    return [
+        format_single_linter_file(file_name, linter_report[file_name])
+        for file_name in linter_report
+    ]
