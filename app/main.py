d062533ed4326628955081e6b5dc228fa8fa8ef2
KevinXD <kevinfdevid@gmail.com>
2022-06-27 12:45:42 +0300

    implemented functions were used in format_single_linter_file and format_linter_report functions

--- app/main.py
+++ app/main.py
@@ -8,25 +8,13 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{"line": error["line_number"],
-                        "column": error["column_number"],
-                        "message": error["text"],
-                        "name": error["code"],
-                        "source": "flake8"
-                        }for error in errors],
+    return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
             "status": "passed" if errors == [] else "failed"
             }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [
-        {"line": error["line_number"],
-         "column": error["column_number"],
-         "message": error["text"],
-         "name": error["code"],
-         "source": "flake8"
-         }for error in linter_report.get(path)],
-        "path": path,
-        "status": "passed" if linter_report.get(path) == [] else "failed"
-    }for path in linter_report]
+    return [format_single_linter_file(path, error_list)
+            for path, error_list in linter_report.items()
+            ]
