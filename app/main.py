7d557d793d2cc68d826d57264e9cde24867379fb
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-06-29 13:50:21 +0300

    Change key and value

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"], "column": error["column_number"],
+        "line": error["line_number"],
+        "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
         "source": "flake8"
@@ -16,5 +17,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
