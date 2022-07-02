ca069864ad9f2b2235bc614e0763dc6eb16ce437
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-07-02 14:53:33 +0300

    MY_Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
-    pass
+    return
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
-    pass
+    return
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
-    pass
+    return
