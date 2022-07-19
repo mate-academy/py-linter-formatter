dbb480516f786ae685015a0e1ca1f82777a4f678
Volodymyr Panov <vpanov25@gmail.com>
2022-07-19 16:00:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-            key: value for (key, value) in
-            zip(["line", "column", "message", "name", "source"],
-            [error["line_number"], error["column_number"], error["text"], error["code"], "flake8"])
+        key: value for (key, value) in
+        zip(["line", "column", "message", "name", "source"],
+            [error["line_number"], error["column_number"],
+             error["text"], error["code"], "flake8"])
     }
 
 
@@ -15,4 +16,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(test, error) for test, error in linter_report.items()]
+    return [format_single_linter_file(test, error) for
+            test, error in linter_report.items()]
