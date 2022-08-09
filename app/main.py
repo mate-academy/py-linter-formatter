f01fb078c540abd8340cc7503266aefd22fa6e1a
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-08-09 17:44:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -13,5 +13,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(path, error)
+            for path, error in linter_report.items()]
