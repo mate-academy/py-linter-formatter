c39de9a99ceab208e028753486fd71c5696a646d
Davyd Kryvko <davidkrivko@gmail.com>
2022-08-12 12:52:44 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 def format_linter_error(error: dict) -> dict:
-
     return {"line": error["line_number"], "column": error["column_number"], "message": error["text"],
             "name": error["code"], "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {"errors": [format_linter_error(eror) for eror in errors], "path": file_path,
-            "status": "passed" if errors == [] else "failed"
-            }
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-
     return [format_single_linter_file(name, erors) for name, erors in linter_report.items()]
