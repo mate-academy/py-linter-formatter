4fb1a2279b8e238773c7ff4fe971176ab1c12372
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-08-16 15:07:04 -0300

    Removed not relevant comments

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
+
     return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
@@ -8,14 +8,14 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
+
     return {"errors": [format_linter_error(error) for error in errors],
             'path': file_path,
             'status': 'failed'}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
+
     return [format_single_linter_file(key, value)
             if value
             else {'errors': [], 'path': key, 'status': 'passed'}
