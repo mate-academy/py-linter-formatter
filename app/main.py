b9aa9af4256e3d3b4a6b334dce3ca4127e4b25e3
Ivan Berezyanskyi <gamaew.res@gmail.com>
2022-08-20 12:16:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ def format_linter_error(error: dict) -> dict:
             'column': error["column_number"],
             'message': error["text"],
             'name': error["code"],
-            'source': "flake8"
-           }
+            'source': "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -15,5 +14,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return[format_single_linter_file(path, set_of_errors)for path, set_of_errors in linter_report.items()]
-
+    return[format_single_linter_file(path, set_of_errors)for path,
+           set_of_errors in linter_report.items()]
