bf982801ae814a5a46d2d86376577430ff76b65d
Artem Dubinin <artemdubinin.ad@gmail.com>
2022-08-22 23:18:11 +0200

    Used implemented functions

--- app/main.py
+++ app/main.py
@@ -10,38 +10,12 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": errors[a]["line_number"],
-                "column": errors[a]["column_number"],
-                "message": errors[a]["text"],
-                "name": errors[a]["code"],
-                "source": "flake8",
-            }
-            for a in range(len(errors))
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "passed" if errors == [] else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": [
-                {
-                    "line": list(linter_report.values())[i][a]["line_number"],
-                    "column":
-                        list(linter_report.values())[i][a]["column_number"],
-                    "message": list(linter_report.values())[i][a]["text"],
-                    "name": list(linter_report.values())[i][a]["code"],
-                    "source": "flake8",
-                }
-                for a in range(len(list(linter_report.values())[i]))
-            ],
-            "path": list(linter_report.keys())[i],
-            "status": "passed" if list(linter_report.values())[i] == []
-            else "failed"
-        }
-        for i in range(len(linter_report))
-    ]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
