5e3b6c3f6f1ef4c79511667b5aa506653080f5ba
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-01 17:44:58 +0300

    Passed_

--- app/main.py
+++ app/main.py
@@ -1,27 +1,19 @@
 def format_linter_error(error: dict) -> dict:
-
     return {
         "column": error["column_number"],
         "line": error["line_number"],
-        "message":  error["text"],
+        "message": error["text"],
         "name": error["code"],
-        "source": "flake8"
-        }
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed"
-        }
+        "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key , value in linter_report.items()]
-
-
-
-
-
+    return [format_single_linter_file(key, value)
+            for key , value in linter_report.items()]
