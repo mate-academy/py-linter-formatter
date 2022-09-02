e270064b38e7b24663c085f2082cdfc3fb060208
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-02 20:12:00 +0300

    final 6

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
-def format_linter_error(errors: dict) -> dict:
-    return {"line": errors["line number"],
-            "column": errors["column_number"],
-            "message": errors["text"],
-            "name": errors["code"],
+def format_linter_error(error: dict) -> dict:
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
             "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
+    return {"errors": [format_linter_error(single_err) for single_err in errors],
             "path": file_path,
-            "status": "passed" if not errors else "failed"}
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(key, value_list)
+            for key, value_list in linter_report.items()]
+            
