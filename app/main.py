970ce7ece7d600dd2aa2ad1cfeec41063dc6fa4e
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2022-09-03 23:40:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ def format_linter_error(error: dict) -> dict:
             "message": error["text"],
             "name": error["code"],
             "source": "flake8"}
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -13,11 +12,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
             "status": "failed" if errors else "passed"}
 
 
-
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
         for (file_path, errors)
         in linter_report.items()
     ]
-
