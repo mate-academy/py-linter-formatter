f0fa5b93c468940ee388f69dce0929dc9e2b5d7e
mltdll <ilya.vyplavin@gmail.com>
2022-09-12 18:36:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {  # Not sure if dict comprehension is supposed to be used here.
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {  # Neither am I sure if I should use comprehension here...
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [  # Oh, finally. This one is clear!
+        format_single_linter_file(file, linter_report[file])
+        for file in linter_report
+    ]
