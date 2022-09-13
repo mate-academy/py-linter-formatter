26236c2d45f48b43787788e98817fe740ff7fa8b
graandda <graanddadays@gmail.com>
2022-09-13 21:14:34 +0300

    Solution - with changes

--- app/main.py
+++ app/main.py
@@ -1,28 +1,23 @@
 def format_linter_error(error: dict) -> dict:
 
-    return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"}
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
 
-    return {
-        "errors": [format_linter_error(error) for error in errors],
-        "path": f"{file_path}",
-        "status": "failed"}
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": f"{file_path}",
+            "status": "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
 
-    return [{"errors": [],
+    return [{"errors": [format_linter_error(error) for error in value],
              "path": f"{key}",
-             "status": "passed"}
-            if value == [] else {
-                "errors": [format_linter_error(error)for error in value],
-                "path": f"{key}",
-                "status": "failed"}
+             "status": "passed" if linter_report[key] == [] else "failed"}
             for key, value in linter_report.items()]
+
