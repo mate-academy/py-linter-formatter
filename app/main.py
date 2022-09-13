d8bf2cfdff26c3130ee62b55d9620ad644556ff6
Alexander <alexzubkoff123@gmail.com>
2022-09-13 20:54:49 +0300

    Fixed brackets position for good code style

--- app/main.py
+++ app/main.py
@@ -19,5 +19,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(file_name, linter_report[file_name])
-            for file_name in linter_report
-            ]
+            for file_name in linter_report]
