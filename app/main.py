cca0c38414a114b4f74dfd3d23c53b15273c20d4
Alexander <alexzubkoff123@gmail.com>
2022-09-13 15:41:32 +0300

    Fixed some items according good style

--- app/main.py
+++ app/main.py
@@ -9,14 +9,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors":
+    return {
+        "errors":
             [format_linter_error(error) for error in errors if errors],
             "path": file_path,
             "status": "failed" if errors else "passed"
-            }
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, linter_report[key])
-            for key in linter_report.keys()
+    return [format_single_linter_file(file_name, linter_report[file_name])
+            for file_name in linter_report
             ]
