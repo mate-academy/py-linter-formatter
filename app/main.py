b71b96ac2911c7c8804898c2f7c1315db3174452
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-14 17:18:12 +0300

    chage kay and value to file_name and error_list

--- app/main.py
+++ app/main.py
@@ -20,6 +20,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
 
     return [
-        format_single_linter_file(key, value)
-        for key, value in linter_report.items()
+        format_single_linter_file(file_name, error_list)
+        for file_name, error_list in linter_report.items()
     ]
