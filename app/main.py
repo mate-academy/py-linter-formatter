56cb50655cecf2424c4dce8112420f919d172597
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-09-15 13:06:33 +0300

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -20,5 +20,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(path, linter_report[path])
-            for path in linter_report
-            ]
+            for path in linter_report]
