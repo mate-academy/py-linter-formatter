05b82c9a1c0f007432f7adcb43914e061a59d428
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-15 10:00:34 +0300

    Fixed code style and variable names

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error.get("line_number"),
-            "column": error.get("column_number"),
-            "message": error.get("text"),
-            "name": error.get("code"),
-            "source": "flake8"
-            }
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -16,5 +17,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file, error)
+            for file, error in linter_report.items()]
