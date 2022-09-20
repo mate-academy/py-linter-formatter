613cbaf43107700af8c3edd1a2a1d580b161f1cd
Igor Pylat <igorpylat@gmail.com>
2022-09-20 04:45:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,56 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return(
+        {
+            "column": error["column_number"],
+            "line": error["line_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8",
+        }
+    )
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return(
+        {
+            "errors":
+            [
+                {
+                    "column": errors[number]["column_number"],
+                    "line": errors[number]["line_number"],
+                    "message": errors[number]["text"],
+                    "name": errors[number]["code"],
+                    "source": "flake8",
+                }
+                for number in range(len(errors))
+            ],
+            "path": file_path,
+            "status": ("failed" if errors else "passed"),
+        }
+    )
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return(
+        [
+            {
+                "errors":
+                [
+                    {
+                        "column": name["column_number"],
+                        "line": name["line_number"],
+                        "message": name["text"],
+                        "name": name["code"],
+                        "source": "flake8",
+                    }
+                    for name in value
+                ],
+                "path": key,
+                "status": ("failed" if value else "passed"),
+            }
+            for key, value in linter_report.items()
+        ]
+    )
     pass
