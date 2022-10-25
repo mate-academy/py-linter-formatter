f784ce9c68d1002b31b487ac0a7d0089a5ebed9a
sergbondckua <sergninja@gmail.com>
2022-10-25 13:36:45 +0300

    add solution and fix flake

--- app/main.py
+++ app/main.py
@@ -17,10 +17,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
-        }
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
 
     return [format_single_linter_file(file, error)
-    for file, error in linter_report.items()]
+            for file, error in linter_report.items()]
