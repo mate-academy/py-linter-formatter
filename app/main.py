c234914116c69156b4f8f647e85ad2628d7329d7
Intellium7 <lomaleks@gmail.com>
2022-10-25 14:01:59 +0300

    fix solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@
 
 def format_linter_error(error: dict) -> dict:
 
-    return dict(
-        line=error["line_number"],
-        column=error["column_number"],
-        message=error["text"],
-        name=error["code"],
-        source="flake8"
-    )
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
