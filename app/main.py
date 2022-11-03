165556117e24ca27e977e890af27c3d657bb1da5
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2022-11-03 22:33:59 +0200

    error correctionÐ

--- app/main.py
+++ app/main.py
@@ -5,17 +5,17 @@ def format_linter_error(error: dict) -> dict:
         "message": error["text"],
         "name": error["code"],
         "source": "flake8"
-           }
+        }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return{"errors": [format_linter_error(char) for char in errors],
+    return {"errors": [format_linter_error(char) for char in errors],
            "path": file_path,
            "status": "passed" if errors == [] else "failed"
-          }
+           }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(file_path, errors)
             for file_path, errors in linter_report.items()
-           ]
+            ]
