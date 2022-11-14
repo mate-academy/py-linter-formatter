6293bded8ade1ad9ebccaa95e7cce0fd850a90b1
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-11-14 22:06:19 +0300

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -8,12 +8,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return ({"errors": [format_linter_error(error) for error in errors],
+    return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
-             "status": "passed" if errors == [] else "failed"})
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return ([
-            format_single_linter_file(key, linter_report[key])
-            for key in linter_report])
+    return [format_single_linter_file(i, linter_report[i])
+            for i in linter_report]
