a405d857d6784a577013c2ba15c0cba7827aaa2b
Anatoliy Pilipey <pilipeytolya@gmail.com>
2022-11-25 20:06:44 +0200

    descript name variabl

--- app/main.py
+++ app/main.py
@@ -18,6 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(ar, linter_report[ar])
-        for ar in linter_report.keys()
+        format_single_linter_file(report, linter_report[report])
+        for report in linter_report.keys()
     ]
