d0e7c6bdc0fc7feb1785254b25182b2838f836b7
Andriy <andriykrav@gmail.com>
2022-12-19 22:02:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,35 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    # reformat single error, replace  keys with new keys
+    # with the same values
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+    # create the dictionary with 3 keys,
+    # key "errors" : value (list of errors)
+    return {
+        "errors":
+            [
+                # call function every time to collect errors
+                format_linter_error(error) for error in errors
+            ],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    # take function format_single_linter_file
+    # with its parameters, and create
+    # new dictionary of arguments from the last function
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
