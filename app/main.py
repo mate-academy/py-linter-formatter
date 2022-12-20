59b60f2a9b9bf43cb85f00f694b03b4c51314b2c
Tania Kharchuk <tkharchuk14@gmail.com>
2022-12-20 21:20:49 +0200

    changed solution2

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [
-        {"line": error["line_number"], "column": error["column_number"],
-         "message": error["text"], "name": error["code"],
-         "source": "flake8"} for error in errors
-    ],
+    return {
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed"}
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [
-        {"line": error["line_number"], "column": error["column_number"],
-         "message": error["text"], "name": error["code"],
-         "source": "flake8"} for error in linter_report[key]
-    ],
-        "path": key,
-        "status": "failed" if linter_report[key] else "passed"}
-        for key in linter_report]
+    return [
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
+    ]
