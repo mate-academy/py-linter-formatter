3340547bdad812b8a6d02dcff2312739bcc1eebd
Tania Kharchuk <tkharchuk14@gmail.com>
2022-12-20 11:11:02 +0200

    changed solution

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [
-        dict(line=error["line_number"], column=error["column_number"],
-             message=error["text"], name=error["code"],
-             source="flake8") for error in errors
+        {"line": error["line_number"], "column": error["column_number"],
+         "message": error["text"], "name": error["code"],
+         "source": "flake8"} for error in errors
     ],
         "path": file_path,
         "status": "failed" if errors else "passed"}
@@ -18,9 +18,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [{"errors": [
-        dict(line=error["line_number"], column=error["column_number"],
-             message=error["text"], name=error["code"],
-             source="flake8") for error in linter_report[key]
+        {"line": error["line_number"], "column": error["column_number"],
+         "message": error["text"], "name": error["code"],
+         "source": "flake8"} for error in linter_report[key]
     ],
         "path": key,
         "status": "failed" if linter_report[key] else "passed"}
