a36829d2e071def17866c848f5a25787d17f5839
Roman <parom95@ukr.net>
2022-12-20 17:26:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(dic) for dic in errors],
+    return {"errors": [format_linter_error(err) for err in errors],
             "path": file_path,
             "status": "failed" if errors != [] else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key, value in linter_report.items()]
+    return [format_single_linter_file(key, value)
+            for key, value in linter_report.items()]
