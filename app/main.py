13aa3ee15a2af457e783e80b5e11ec2eadcddce9
vito <vitalytyagunov@gmail.com>
2023-01-12 10:32:47 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,40 +1,28 @@
 def format_linter_error(error: dict) -> dict:
-    return {("line" if error_name == "line_number"
-            else "column" if error_name == "column_number"
-            else "message" if error_name == "text"
-            else "name" if error_name == "code"
-            else "source" if error_name == "physical_line"
-            else None): ("flake8" if error_name == "physical_line"
-            else error_body)
-            for (error_name, error_body) in error.items()
-            if error_name != "filename"}
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{("line" if error_name == "line_number"
-            else "column" if error_name == "column_number"
-            else "message" if error_name == "text"
-            else "name" if error_name == "code"
-            else "source" if error_name == "physical_line"
-            else None): ("flake8" if error_name == "physical_line"
-            else error_body)
-            for (error_name, error_body) in error.items()
-            if error_name != "filename"} for error in errors],
+    return {"errors": [{"line": error["line_number"],
+                        "column": error["column_number"],
+                        "message": error["text"],
+                        "name": error["code"],
+                        "source": "flake8"} for error in errors],
             "path": file_path,
             "status": ("failed" if len(errors) > 0 else "passed")}
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [{("line" if error_name == "line_number"
-                    else "column" if error_name == "column_number"
-                    else "message" if error_name == "text"
-                    else "name" if error_name == "code"
-                    else "source"): ("flake8" if error_name == "physical_line"
-                    else error_body)
-                    for (error_name, error_body) in error.items()
-                    if error_name != "store and decorate numbers:"}
-                    for error in list_of_errors],
+        {"errors": [{"line": error["line_number"],
+                     "column": error["column_number"],
+                     "message": error["text"],
+                     "name": error["code"],
+                     "source": "flake8"} for error in list_of_errors],
          "path": path_to_file,
          "status": ("failed" if len(list_of_errors) > 0 else "passed")}
         for (path_to_file, list_of_errors) in linter_report.items()]
