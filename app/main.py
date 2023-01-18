45b6be225ea124443f3116dc0f3f64240b2bcad2
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-01-18 20:45:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,26 +7,6 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
-# error = {
-#     "code": "E501",
-#     "filename": "./source_code_2.py",
-#     "line_number": 18,
-#     "column_number": 80,
-#     "text": "line too long (99 > 79 characters)",
-#     "physical_line": '    return f"I like to filter, rounding, doubling, '
-#     "store and decorate numbers: {', '.join(items)}!\"",
-# }
-#
-# print(format_linter_error(error=error))
-# The output will be:
-# {
-#     "line": 18,
-#     "column": 80,
-#     "message": "line too long (99 > 79 characters)",
-#     "name": "E501",
-#     "source": "flake8"
-# }
-
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
@@ -49,51 +29,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         else "passed"
     }
 
-# errors = [
-#     {
-#         "code": "E501",
-#         "filename": "./source_code_2.py",
-#         "line_number": 18,
-#         "column_number": 80,
-#          "text": "line too long (99 > 79 characters)",
-#         "physical_line": '    return f"I like to filter, rounding, doubling, '
-#         "store and decorate numbers: {', '.join(items)}!\"",
-#     },
-#     {
-#         "code": "W292",
-#         "filename": "./source_code_2.py",
-#         "line_number": 18,
-#         "column_number": 100,
-#         "text": "no newline at end of file",
-#         "physical_line": '    return f"I like to filter, rounding, doubling, '
-#         "store and decorate numbers: {', '.join(items)}!\"",
-#     },
-# ]
-
-# print(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
-# The output will be:
-# {
-#     "errors":
-#         [
-#             {
-#                 "line": 18,
-#                 "column": 80,
-#                 "message": "line too long (99 > 79 characters)",
-#                 "name": "E501",
-#                 "source": "flake8"
-#             },
-#             {
-#                 "line": 18,
-#                 "column": 100,
-#                 "message": "no newline at end of file",
-#                 "name": "W292",
-#                 "source": "flake8"
-#             }
-#         ],
-#     "path": "./source_code_2.py",
-#     "status": "failed"
-# }
-
 
 def format_linter_report(linter_report: dict) -> list:
     return [
@@ -114,59 +49,3 @@ def format_linter_report(linter_report: dict) -> list:
         }
         for errors, values in linter_report.items()
     ]
-
-# report_file = {
-#     "./test_source_code_2.py": [],
-#     "./source_code_2.py":
-#         [
-#             {
-#                 "code": "E501",
-#                 "filename": "./source_code_2.py",
-#                 "line_number": 18,
-#                 "column_number": 80,
-#                 "text": "line too long (99 > 79 characters)",
-#                 "physical_line": '    return f"I like to filter, rounding, doubling, '
-#                 "store and decorate numbers: {', '.join(items)}!\"",
-#             },
-#             {
-#                 "code": "W292",
-#                 "filename": "./source_code_2.py",
-#                 "line_number": 18,
-#                 "column_number": 100,
-#                 "text": "no newline at end of file",
-#                 "physical_line": '    return f"I like to filter, rounding, doubling, '
-#                 "store and decorate numbers: {', '.join(items)}!\"",
-#             },
-#         ]
-# }
-#
-# print(format_linter_report(linter_report=report_file))
-# The output will be:
-# [
-#     {
-#         "errors": [],
-#         "path": "./test_source_code_2.py",
-#         "status": "passed"
-#     },
-#     {
-#         "errors":
-#             [
-#                 {
-#                     "line": 18,
-#                     "column": 80,
-#                     "message": "line too long (99 > 79 characters)",
-#                     "name": "E501",
-#                     "source": "flake8"
-#                 },
-#                 {
-#                     "line": 18,
-#                     "column": 100,
-#                     "message": "no newline at end of file",
-#                     "name": "W292",
-#                     "source": "flake8"
-#                 }
-#             ],
-#         "path": "./source_code_2.py",
-#         "status": "failed"
-#     }
-# ]
