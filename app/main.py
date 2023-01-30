fcb633751fe1c183d4136829d33e391a85ee96f9
bohdanlysuchenko <lysachenko111@ukr.net>
2023-01-30 18:03:53 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(i) for i in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file_path, error)
+            for file_path, error in linter_report.items()]
