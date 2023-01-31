0822fb9aa21ec71aeda0f7d160ca9de39dac0db9
kuznetsb <kuznetsbog@gmail.com>
2023-01-31 22:33:34 +0200

    don't repeat yourself done

--- app/main.py
+++ app/main.py
@@ -12,13 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
         [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
@@ -28,20 +22,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors":
-            [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
-            "path": file,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(file, errors)
         for file, errors in linter_report.items()
     ]
