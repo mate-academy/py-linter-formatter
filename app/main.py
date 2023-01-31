b7997bca14c0a1935d7dae8c8cb1f2762bb53b70
Paul Savchuk <nekrjkeee@gmail.com>
2023-01-31 20:32:25 +0200

    Reworked all var_names for providing more information

--- app/main.py
+++ app/main.py
@@ -7,12 +7,16 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(i) for i in errors],
-            "path": file_path,
-            "status": "failed"} if errors else \
-           {"errors": [], "path": file_path, "status": "passed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed"
+    } if errors else \
+        {"errors": [],
+         "path": file_path,
+         "status": "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(path, errors)
+            for path, errors in linter_report.items()]
