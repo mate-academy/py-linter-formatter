40564a98decc867ed28992f70c42fbb5339b7ed7
Kateryna Kushnir <katerynakushnir3@gmail.com>
2023-02-01 21:52:25 +0000

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["physical_line"],
+            "source": error["store and decorate numbers"]}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    result = {"errors": [format_linter_error(errors[index])] for index in enumerate(errors)}
+    result["path"] = file_path
+    result["status"] = "failed" if errors != [] else "passed"
+    return result
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(element) for element in linter_report]
