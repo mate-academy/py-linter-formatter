c51f06fef9310c208999407accabac18d96d3c6d
Kateryna Kushnir <katerynakushnir3@gmail.com>
2023-02-01 22:15:41 +0000

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def format_linter_error(error: dict) -> dict:
     return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
-            "name": error["physical_line"],
-            "source": error["store and decorate numbers"]}
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
