07f221aa27641759d1cb7ee875c064b875e5dd53
Zenon <zenion@ukr.net>
2023-02-01 21:06:58 +0200

    Fix indentation

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
             format_linter_error(error)
-            for error in errors if error["filename"] == file_path
+            for error in errors if error.get("filename") == file_path
         ],
         "path": file_path,
         "status": "failed" if errors else "passed"
