b44a8ebf041ce771c92ef3961e26a6e39564c5b8
oithegod <serejathegod@gmail.com>
2023-02-01 15:47:57 +0200

    Remove pass keywords

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def format_linter_error(error: dict) -> dict:
         "name": error["code"],
         "source": "flake8"
     }
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -18,4 +17,3 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(path, errors)
             for path, errors in linter_report.items()]
-    pass
