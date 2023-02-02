658d88c7384560bc66218b1bfdc4b388f2da0ea4
Oleksandr Leha <legus96@gmail.com>
2023-02-02 14:42:28 +0200

    'Solution_with_items'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-            }
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-            }
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(dictionary, linter_report[dictionary])
-            for dictionary in linter_report]
+    return [format_single_linter_file(path, error)
+            for path, error in linter_report.items()]
