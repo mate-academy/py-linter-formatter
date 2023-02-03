80ee7d5aced0158cb595c11defcd21bb4b65bf26
kostya-kononenko <kkononenko3@gmail.com>
2023-02-03 18:06:59 +0200

    fix_some_errors_v1

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error.get("line_number"),
+    return {
+            "line": error.get("line_number"),
             "column": error.get("column_number"),
             "message": error.get("text"),
             "name": error.get("code"),
-            "source": "flake8"}
+            "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path=file_path, errors=errors)
-            for file_path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(file_path=file_path, errors=errors)
+        for file_path, errors in linter_report.items()
+    ]
