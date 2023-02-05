cd86a3b888d0e370205ce575c71a27e7e2d84df0
Misha <michaeltwins@gmail.com>
2023-02-06 00:49:54 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -14,13 +14,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
             [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
-    }  # why we cannot use here expression "... if errors is True else ..." ?
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
-        # if "path" != "passed" else
-        # {"errors": [], "path": "path", "status": "passed"}
         for file_path, errors in linter_report.items()
     ]
