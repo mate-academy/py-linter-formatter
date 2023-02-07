6c0862126eb6b4d68929f47de9adbf81ae51cf6f
Ruslan <noezyboy1@gmail.com>
2023-02-07 16:26:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,63 +1,21 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": 18,
-        "column": 80,
-        "message": "line too long (99 > 79 characters)",
-        "name": "E501",
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8"
     }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path: str, errors: dict) -> dict:
     return {
-        "errors":
-        [
-            {
-                "line": 18,
-                "column": 80,
-                "message": "line too long (99 > 79 characters)",
-                "name": "E501",
-                "source": "flake8"
-            },
-            {
-                "line": 18,
-                "column": 100,
-                "message": "no newline at end of file",
-                "name": "W292",
-                "source": "flake8"
-            }
-        ],
-        "path": "./source_code_2.py",
-        "status": "failed"
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-    {
-        "errors": [],
-        "path": "./test_source_code_2.py",
-        "status": "passed"
-     },
-        {
-        "errors":
-            [
-                {
-                    "line": 18,
-                    "column": 80,
-                    "message": "line too long (99 > 79 characters)",
-                    "name": "E501",
-                    "source": "flake8"
-                },
-                {
-                    "line": 18,
-                    "column": 100,
-                    "message": "no newline at end of file",
-                    "name": "W292",
-                    "source": "flake8"
-                }
-            ],
-        "path": "./source_code_2.py",
-        "status": "failed"
-    }
-    ]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
