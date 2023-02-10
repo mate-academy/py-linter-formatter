c6c5ce008e672856bcf1ffc5f8084a3d964e2ca2
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-10 16:55:27 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -8,20 +8,19 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
     return {
         "errors": [
             format_linter_error(err)
             for err in errors
-            if err["filename"] == file_path
         ],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
 
 
-def format_linter_report(linter_report: dict) -> list:
+def format_linter_report(linter_report: dict) -> list[dict]:
     return [
-        format_single_linter_file(key, value)
-        for key, value in linter_report.items()
+        format_single_linter_file(error, linter_report[error])
+        for error in linter_report.keys()
     ]
