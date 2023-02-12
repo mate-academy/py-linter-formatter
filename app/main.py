3b774d195777f8af4e05ea5681242c493967dd65
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-02-13 00:40:23 +0200

    flake8 fix

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key_file_path, value_errors)
-            for key_file_path, value_errors
-            in linter_report.items()]
+    return [
+        format_single_linter_file(key_file_path, value_errors)
+        for key_file_path, value_errors in linter_report.items()
+    ]
