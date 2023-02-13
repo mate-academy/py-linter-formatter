f076aade1baf0392df0903a55fb46e7c7c8c5436
Volodymyr Yerokhov <kavbian@gmail.com>
2023-02-13 22:37:07 +0200

    'dont_repeat_yourself'

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
     return {
         "errors": [
-            {
-                "line": error.get("line_number"),
-                "column": error.get("column_number"),
-                "message": error.get("text"),
-                "name": error.get("code"),
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
@@ -27,20 +21,6 @@ def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error.get("line_number"),
-                    "column": error.get("column_number"),
-                    "message": error.get("text"),
-                    "name": error.get("code"),
-                    "source": "flake8"
-                }
-                for error in linter_report[path]
-                if linter_report.get(path)
-            ],
-            "path": path,
-            "status": "failed" if linter_report.get(path) else "passed"
-        }
+        format_single_linter_file(path, linter_report.get(path))
         for path in linter_report
     ]
