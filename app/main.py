8d4cf68e5bcc83e799490b60eb55d14e6f264355
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2023-02-15 13:29:38 +0200

    Task_is_done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-from audioop import error
-
 
 def format_linter_error(error: dict) -> dict:
-
     return {
         "line": error.get("line_number"),
         "column": error.get("column_number"),
@@ -13,32 +10,16 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {
         "errors":
-            [
-                {
-                    format_linter_error(error)
-                }
-            ],
-        "path": "./source_code_2.py",
-        "status": "failed"
+            [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if len(errors) > 0 else "passed"
     }
 
-def format_linter_report(linter_report: dict) -> list:
 
+def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [],
-            "path": "./test_source_code_2.py",
-            "status": "passed"
-        },
-        {
-            "errors":
-                [
-                    {
-                        format_single_linter_file(file_path, error)
-                    }
-                ]
-        }
+        format_single_linter_file(key, value)
+        for (key, value) in linter_report.items()
     ]
