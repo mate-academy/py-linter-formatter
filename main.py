0accb3d2fbba7e79d121f8884a6ecdc8a79bff3d
Petro Havrysh <petro.havrysh@gmail.com>
2023-02-20 17:56:42 +0200

    'Solution'

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+def format_linter_error(error: dict) -> dict:
+    # write your code here
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def format_single_linter_file(file_path: str, errors: list) -> dict:
+    # write your code here
+    formatted_errors = []
+    for error in errors:
+        formatted_errors.append(format_linter_error(error))
+
+    status = "failed" if formatted_errors else "passed"
+
+    return {
+        "path": file_path,
+        "errors": formatted_errors,
+        "status": status
+    }
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def format_linter_report(linter_report: dict) -> list:
+    # write your code here
+    output = []
+    for file_path, errors in linter_report.items():
+        error_list = [format_linter_error(error) for error in errors]
+        status = "passed" if not error_list else "failed"
+        output.append({
+            "path": file_path,
+            "errors": error_list,
+            "status": status
+        })
+    return output
