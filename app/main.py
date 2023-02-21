6dd96d4eed648d0b9129072df9d731210d8456e4
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-02-21 11:55:37 +0200

    Select2

--- app/main.py
+++ app/main.py
@@ -3,20 +3,21 @@ def format_linter_error(error: dict) -> dict:
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
-        "name": error["code"], "source": "flake8"
+        "name": error["code"],
+        "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(error1) for error1 in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if errors == [] else "failed"
+        "status": "passed" if len(errors) == 0 else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(key, linter_report[key])
-        for key in linter_report
+        format_single_linter_file(files, errors)
+        for files, errors in linter_report.items()
     ]
