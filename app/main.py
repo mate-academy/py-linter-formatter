c9622e83621f327d0414929727c99eb781eb2ba3
maccarets <maccarets@gmail.com>
2023-02-21 11:59:35 +0200

    almost done

--- app/main.py
+++ app/main.py
@@ -25,5 +25,17 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+        "errors": [{
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+        } for error in errors],
+
+        "path": key,
+        "status": "failed"
+
+    } for key, errors in linter_report.items()]
