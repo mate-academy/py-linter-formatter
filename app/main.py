582a279cc5669eae193be91fb5f05d469ce279da
Natalia Stepanenko <natti8006@gmail.com>
2023-03-03 16:09:47 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(cur_error) for cur_error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors != [] else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path=key, errors=value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file_path=file_path, errors=errors)
+            for file_path, errors in linter_report.items()]
