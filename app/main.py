0e70020756eeffa23390c6bd3e6aee0d1dcba4c9
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-03-04 16:24:31 +0200

    Linter report convertion functions refactored

--- app/main.py
+++ app/main.py
@@ -10,19 +10,14 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            format_linter_error(current_error)
-            for current_error in errors if current_error["filename"]
-            == file_path],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{
-        "errors": (error_result := format_single_linter_file(file_name,
-            error_list)["errors"]),
-        "path": file_name,
-        "status": "failed" if len(error_result) else "passed"
-    } for file_name, error_list in linter_report.items()]
+    return [
+        format_single_linter_file(file_name, error_list)
+        for file_name, error_list in linter_report.items()
+    ]
