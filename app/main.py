72ccf633a1a325c3e9f982ae2cc836934e8acc57
Alim Selemetov <alimselemetow@gmail.com>
2023-03-21 09:28:02 +0300

    fix changes

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number", None),
+        "column": error.get("column_number", None),
+        "message": error.get("text", None),
+        "name": error.get("code", None),
         "source": "flake8"
     }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": ("failed" if errors != [] else "passed")
+        "status": "failed" if errors != [] else "passed"
     }
 
 
-def format_linter_report(linter_report: dict) -> list:
+def format_linter_report(linter_report: dict) -> list[dict]:
     return [
-        format_single_linter_file(key, linter_report[key])
+        format_single_linter_file(key, linter_report.get(key, None))
         for key in linter_report
     ]
