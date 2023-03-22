b67d1ddf0ba1571c81bf678893c136d0f42931bd
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-03-22 15:37:09 +0200

    Rename 'dic' for 'key'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"} \
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    } \
         if len(error) > 0 else {}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(key) for key in errors],
-            "path": file_path,
-            "status": "failed"
-            if len(errors) > 0 else "passed"}
+    return {
+        "errors": [format_linter_error(key) for key in errors],
+        "path": file_path,
+        "status": "failed"
+        if len(errors) > 0 else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(index, linter_report[index])
-            for index in linter_report]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
