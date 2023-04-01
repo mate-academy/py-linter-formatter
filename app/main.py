8bf76b3d85f66decb3e3dc15aab6b1cc4e480052
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-04-01 15:31:37 +0200

    work with mistake

--- app/main.py
+++ app/main.py
@@ -6,18 +6,15 @@ def format_linter_error(error: dict) -> dict:
         "name": error.get("code"),
         "source": "flake8"
     }
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": ("failed" if len(errors) > 0 else "passed")
+        "status": ("failed" if errors else "passed")
     }
-    pass
-
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(k, v) for k, v in linter_report.items()]
+    return [format_single_linter_file(key, value) for key, value in linter_report.items()]
