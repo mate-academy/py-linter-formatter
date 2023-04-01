c9e4774a1ffaf746fcc922193e523a45d320d4c5
Bohdan Zakora <b.v.zakora@gmail.com>
2023-04-01 15:57:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+    # створює словник зі зміненими ключами та додатковим полем
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status":
+            "failed" if [format_linter_error(error) for error in errors]
+            else "passed"
+    }
+    # в "errors" записує, змінену попередньою функцією, інформацію з errors
+    # в "path" передає шлях файлу з file_path
+    # в "status" передає інформацію чи є взагалі помилка
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
+    # форматує всі помилки для всіх файлів звіту
