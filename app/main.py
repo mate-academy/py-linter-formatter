aa131490313833c69ca5a2902a8ec050b6d98139
Stanislav Bulanov <p99kab00@gmail.com>
2023-04-02 13:03:31 +0300

    Remove function docstrings from the module

--- app/main.py
+++ app/main.py
@@ -5,14 +5,6 @@ LINTER_NAME = "flake8"
 
 
 def format_linter_error(error: dict) -> dict:
-    """Format error to a linter specific report.
-
-    Args:
-        error - the error information
-
-    Returns:
-        A dict with formatted error
-    """
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -23,15 +15,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    """Format single file error to a linter specific report.
-
-    Args:
-        file_path - a file path
-        errors - file formatting errors
-
-    Returns:
-        A dict with file path, linter status, and a list of errors
-    """
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
@@ -40,14 +23,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    """Format multiple files error to a linter specific report.
-
-    Args:
-        linter_report - files with errors
-
-    Returns:
-        A list of dicts with file path, linter status, and a list of errors
-    """
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
