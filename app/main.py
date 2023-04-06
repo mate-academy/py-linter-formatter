b1b7b24e491b03e48e6804dad17812b0a18d46ae
Volodymyr Vilisov <vilisovv@gmail.com>
2023-04-06 13:58:09 +0200

    Without repeat

--- app/main.py
+++ app/main.py
@@ -14,13 +14,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                } for error in errors
+                format_linter_error(error) for error in errors
             ],
         "path": file_path,
         "status": "failed" if len(errors) > 0 else "passed"
