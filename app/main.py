b07d13541fdcea1032d72d19ac5954359ec4779b
Anton Hryhorenko <grygorenko23@gmail.com>
2023-04-11 15:46:20 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -18,6 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path=dict_key, errors=dict_value)
-        for dict_key, dict_value in linter_report.items()
+        format_single_linter_file(file_path=file_path, errors=errors)
+        for file_path, errors in linter_report.items()
     ]
