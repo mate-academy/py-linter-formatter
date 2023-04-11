ed73ffa391cc26fb171e0037889c7ad2c0609020
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-04-11 09:47:47 +0300

    Fixed py-linter-formatter

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
@@ -27,19 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in file_errors
-            ],
-            "path": file_path,
-            "status": "failed" if file_errors else "passed"
-        }
-        for file_path, file_errors in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
