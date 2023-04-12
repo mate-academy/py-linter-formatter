934596149a093fdf9667ca15cef10f825f13d0c3
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-04-12 12:04:32 +0300

    Use get() method and change 15 line

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
@@ -12,7 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if len(errors) > 0 else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
