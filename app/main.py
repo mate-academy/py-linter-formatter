e97eaeb8d4c5f07edbdb33ffaf3e2df0c21f8276
Evgen Milokumov <evgen.milok@gmail.com>
2023-04-28 16:09:05 +0300

    Milokumov-Solution/fix1

--- app/main.py
+++ app/main.py
@@ -4,17 +4,14 @@ def format_linter_error(error: dict) -> dict:
             "message": error["text"],
             "name": error["code"],
             "source": "flake8"}
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(value) for value in errors],
+    return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
             "status": "failed" if errors else "passed"}
-    pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
-    pass
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
