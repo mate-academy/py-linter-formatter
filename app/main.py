d7b51502fba476aa635d01e3c1bc20ad6bf8d093
ZUBOV ILLIA <zubovillia@gmail.com>
2023-05-08 08:56:04 +0300

    removed copying of code instead use functions

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
@@ -27,19 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in linter_report[key]
-            ],
-            "path": key,
-            "status": "failed" if len(linter_report[key]) else "passed"
-        }
-        for key in linter_report
+        format_single_linter_file(file_path, linter_report[file_path])
+        for file_path in linter_report
     ]
