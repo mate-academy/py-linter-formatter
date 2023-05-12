0813106be73d7b7a710e70348a70ad3871400d21
Mikhail <tiunovmixs@gmail.com>
2023-05-12 22:55:11 +0300

    Correction: format_linter_error

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,10 +9,8 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
     pass
