d44207c437998356afb5a19c57455bd7e2030792
Mikhail <tiunovmixs@gmail.com>
2023-05-13 16:30:41 +0300

    Fixes with duplicated code in format_single_linter_file and format_linter_report

--- app/main.py
+++ app/main.py
@@ -10,40 +10,20 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                {
-                    "line": err["line_number"],
-                    "column": err["column_number"],
-                    "message": err["text"],
-                    "name": err["code"],
-                    "source": "flake8"}
-                for err in errors],
+        "errors": [format_linter_error(err) for err in errors],
         "path": file_path,
-        "status": "failed"
-        if errors else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors":
-                [
-                    {
-                        "line": item["line_number"],
-                        "column": item["column_number"],
-                        "message": item["text"],
-                        "name": item["code"],
-                        "source": "flake8"}
-                    for item in linter_report[corr]
-                    if item["filename"] in corr and item["text"] and item["code"]],
+            "errors": [format_linter_error(item) for item in linter_report[corr]
+                       if item["filename"] in corr and item["text"] and item["code"]],
             "path": corr,
-            "status": "failed"
-            if any(item["text"] and item["code"]
-                   for item in linter_report[corr])
-            else "passed"
-
+            "status": "failed" if any(item["text"] and item["code"]
+                                      for item in linter_report[corr]) else "passed"
         }
         for corr in linter_report
     ]
