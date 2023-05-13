6fdee27aaa54fd3f8be402e0470a855d47b34d38
Michael <manchester6913@gmail.com>
2023-05-13 06:21:10 -0400

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -19,5 +19,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(k, v) for k, v in linter_report.items()
+        format_single_linter_file(path, status)
+        for path, status in linter_report.items()
     ]
