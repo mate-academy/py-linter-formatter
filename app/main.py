4f288507ca75bf5a9f37b4911373816ab8091153
Maksym Dubov <dubovwork7@gmail.com>
2023-05-15 13:50:07 +0300

    1

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
@@ -14,6 +15,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
                 format_linter_error(error) for error in errors
             ], "path": file_path, "status": "passed" if "errors" == [] else "failed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [
