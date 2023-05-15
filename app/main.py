fc3531b5d61dae87eedafd41f5a26bb14d6476d7
Maksym Dubov <dubovwork7@gmail.com>
2023-05-15 15:39:57 +0300

    1

--- app/main.py
+++ app/main.py
@@ -10,13 +10,14 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                format_linter_error(error) for error in errors
-            ], "path": file_path, "status": "passed" if "errors" == []
-            else "failed"}
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if "errors" == [] else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors) for path,
-            errors in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
