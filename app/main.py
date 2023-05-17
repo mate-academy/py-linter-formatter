fc6d71a75b5a6e3095bd96779326f2ce9f50a7e8
Valdyslav <magnetto54@gmail.com>
2023-05-17 15:57:55 +0200

    Fixed code style and line 15

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     # Change error type by file
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
-            "status": "passed" if errors == [] else "failed"
+            "status": "passed" if not errors else "failed"
             }
 
 
 def format_linter_report(linter_report: dict) -> list:
     # Grouping files into a list
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file_name, file_errors)
+            for file_name, file_errors in linter_report.items()]
