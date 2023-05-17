3c6c57101ff031a5c232a62a7b30f30426149b2c
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-05-17 15:32:20 +0300

    Fixed: Used get() method. Functions format_linter_error(), format_single_linter_file()

--- app/main.py
+++ app/main.py
@@ -1,26 +1,16 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if len(errors) > 0 else "passed"
     }
@@ -28,19 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors":
-                [
-                    {
-                        "line": value["line_number"],
-                        "column": value["column_number"],
-                        "message": value["text"],
-                        "name": value["code"],
-                        "source": "flake8"
-                    }
-                    for value in linter_report[report]],
-            "path": report,
-            "status": "failed" if len(linter_report[report]) > 0 else "passed"
-        }
+        format_single_linter_file(report, linter_report.get(report))
         for report in linter_report
     ]
