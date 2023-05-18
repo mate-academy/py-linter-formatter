f17e697f05b3bb7b54e4d6163c0c45886d647759
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-05-18 13:59:02 +0300

    bugs fixed

--- app/main.py
+++ app/main.py
@@ -1,41 +1,22 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [
-        {
-            "line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-        }
-        for error in errors],
-        "path": file_path,
-        "status": "failed"
-    }
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path,
+            "status": "failed" if errors else "passed"
+            }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors":
-            [{
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-                for error in errors
-            ],
-            "path": file,
-            "status": "passed" if len(errors) == 0 else "failed"}
-        for file, errors in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
