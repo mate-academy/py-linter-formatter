a3a782025f86fb8f558e1a0b4c2d614c6b985e10
Valdyslav <magnetto54@gmail.com>
2023-05-19 16:59:42 +0200

    Fixed dictionary format

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 def format_linter_error(error: dict) -> dict:
     # Error type correction
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-            }
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
