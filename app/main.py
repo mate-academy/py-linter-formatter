a3756d1ae0e8379b0f671c8af09818fdb2bfa6c1
Valdyslav <magnetto54@gmail.com>
2023-05-19 17:01:12 +0200

    Fixed dictionary format

--- app/main.py
+++ app/main.py
@@ -11,10 +11,11 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # Change error type by file
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "passed" if not errors else "failed"
-            }
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if not errors else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
