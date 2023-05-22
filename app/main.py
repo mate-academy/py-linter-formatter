088d74624506e92f0e8e49eba846ebae00bfda9f
YOUR_NAME <YOUR_EMAIL>
2023-05-22 18:47:10 +0300

    Errors in dict (task #3(rename file_path=path, errors=errors))

--- app/main.py
+++ app/main.py
@@ -23,6 +23,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
 
     return [
-        format_single_linter_file(file_path=key, errors=value)
-        for key, value in linter_report.items()
+        format_single_linter_file(file_path=path, errors=errors)
+        for path, errors in linter_report.items()
     ]
