40353e0c7018828dccbbfabe85e4f78bd3f0a972
YOUR_NAME <YOUR_EMAIL>
2023-05-22 18:39:40 +0300

    Errors in dict (task #3(delete comments))

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+
     result_list = {
         "errors": errors,
         "path": file_path,
@@ -21,5 +21,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    return [format_single_linter_file(file_path=key, errors=value) for key, value in linter_report.items()]
+
+    return [
+        format_single_linter_file(file_path=key, errors=value)
+        for key, value in linter_report.items()
+    ]
