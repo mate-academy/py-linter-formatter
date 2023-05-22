e998eb60832af6c3a5322909e07414086b2002f5
maccarets <maccarets@gmail.com>
2023-05-22 13:12:06 +0300

    done

--- app/main.py
+++ app/main.py
@@ -11,31 +11,30 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [{
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
         } for error in errors],
 
         "path": file_path,
         "status": "failed"
-
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-        "errors": [{
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"
-        } for error in errors],
+            "errors": [{
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            } for error in errors],
 
-        "path": key,
-        "status": "failed"
+            "path": key,
+            "status": "failed" if len(errors) > 0 else "passed"
 
-    } for key, errors in linter_report.items()]
+        } for key, errors in linter_report.items()]
