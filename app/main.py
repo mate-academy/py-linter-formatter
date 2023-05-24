0e25d6875f0e86851d27901c77ac92f03013d66b
arturiermolenko <arturiermolenko@gmail.com>
2023-05-24 11:03:40 +0200

    DoneAgain

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
@@ -12,13 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
+                format_linter_error(error)
                 for error in errors
             ],
         "path": file_path,
@@ -33,20 +27,6 @@ def format_linter_report(linter_report: dict) -> list:
             "path": key,
             "status": "passed"
         } if len(value) == 0 else
-        {
-            "errors":
-            [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in value
-            ],
-            "path": key,
-            "status": "failed"
-        }
+        format_single_linter_file(file_path=key, errors=value)
         for key, value in linter_report.items()
     ]
