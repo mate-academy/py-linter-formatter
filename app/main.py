e22cfcfca9f4ddd19d8e169dfb769492953ab599
Alex Yatsun <starcrafter4444@gmail.com>
2023-05-24 16:16:55 +0300

    Improve code

--- app/main.py
+++ app/main.py
@@ -12,12 +12,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if len(errors) == 0 else "failed"
+        "status": "passed" if errors == [] else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(key, linter_report[key])
-        for key in linter_report
+        format_single_linter_file(file_path, linter_report[file_path])
+        for file_path in linter_report
     ]
