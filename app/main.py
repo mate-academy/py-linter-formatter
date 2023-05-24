b02bcd0abbabdd3279275ebbcfdd936281d14eba
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-05-24 15:14:37 +0300

    Deleted duplication

--- app/main.py
+++ app/main.py
@@ -12,11 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                {"line": error["line_number"],
-                 "column": error["column_number"],
-                 "message": error["text"],
-                 "name": error["code"],
-                 "source": "flake8"} for error in errors
+                format_linter_error(error) for error in errors
             ],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
@@ -25,19 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in linter_report[file]
-            ],
-            "path": file,
-            "status": "failed" if linter_report[file] else "passed"
-        }
+        format_single_linter_file(file, linter_report[file])
         for file in linter_report
     ]
