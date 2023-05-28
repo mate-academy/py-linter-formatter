1d4510ecb53c93073988f401119418fef8d05473
Andrii Hrabovyi <mrmestage25@gmail.com>
2023-05-28 15:10:40 +0300

    changes

--- app/main.py
+++ app/main.py
@@ -28,21 +28,3 @@ def format_linter_report(linter_report: dict) -> list:
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
-# return [
-#         {
-#             "errors":
-#             [
-#                 {
-#                     "line": error["line_number"],
-#                     "column": error["column_number"],
-#                     "message": error["text"],
-#                     "name": error["code"],
-#                     "source": "flake8"
-#                 }
-#             ],
-#             "path": file_path,
-#             "status": "failed" if errors else "passed"
-#         }
-#         for file_path, errors in linter_report.items()
-#         for error in errors
-#     ]
