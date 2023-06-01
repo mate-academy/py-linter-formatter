6b4940c63d5b26276123884c3a2321363c8f81eb
Yuliia Verbivska <88626109+verbik@users.noreply.github.com>
2023-06-01 14:14:57 +0300

    Update app/main.py
    Co-authored-by: ArsenPidhoretskyi <65166203+ArsenPidhoretskyi@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -19,5 +19,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(error, path)
-        for error, path in linter_report.items()
+        for path, errors in linter_report.items()
     ]
