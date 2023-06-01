2619fcdea820840e3d95b99c9d4bce1ae745b581
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-06-01 19:09:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,52 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "errors":
+            [
+                {
+                    "line": dc["line_number"],
+                    "column": dc["column_number"],
+                    "message": dc["text"],
+                    "name": dc["code"],
+                    "source": "flake8"
+                }
+                for dc in errors
+            ],
+        "path": file_path,
+        "status": "failed" if any(errors) else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+
+    return [
+        {
+            "errors":
+                [
+                    {
+                        "line": elem["line_number"],
+                        "column": elem["column_number"],
+                        "message": elem["text"],
+                        "name": elem["code"],
+                        "source": "flake8"
+                    }
+                    for elem in val
+                ]
+            if all(val) else [],
+            "path": key,
+            "status":"failed" if any(val)
+            else "passed"
+        }
+        for key, val in linter_report.items()
+    ]
