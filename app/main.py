6eb38cd02d6c3b3dd9031e0b3bd2c4e8f06d07a9
mark.shapka <mark.shapka1@gmail.com>
2023-06-05 14:15:58 +0300

    delete pycache

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if len([format_linter_error(error)
-                                   for error in errors]) == 0 else "failed"
+        "status": "passed" if not errors else "failed"
     }
 
 
