e28d450d538aa5568d5e6e051f2b3e067eea8169
Alla Kuksa <alla.kuksa92@gmail.com>
2023-06-09 13:57:31 +0300

    Correct reuse function

--- app/main.py
+++ app/main.py
@@ -11,38 +11,13 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                {"line": error["line_number"],
-                 "column": error["column_number"],
-                 "message": error["text"],
-                 "name": error["code"],
-                 "source": "flake8"}
-
-                for error in errors],
-
-            "path": file_path,
-            "status": "failed" if errors else "passed"
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": [
-                {
-                    "line": report["line_number"],
-                    "column": report["column_number"],
-                    "message": report["text"],
-                    "name": report["code"],
-                    "source": "flake8"
-                }
-                for report in reports
-            ],
-            "path": path,
-            "status": "failed" if reports else "passed"
-        }
-        for path, reports in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
     pass
