9413470f594bb1ba5b2b115f82121fea617c1264
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-06-11 20:46:07 +0100

    Solution for py linter formatter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {["name", "message", "column", "line", "source"]
+            [a]:
+                [[error. get(i) for i in
+                  ["code", "text", "column_number", "line_number"]]
+                 + ["flake8"]][0][a] for a in range(5)}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"errors": [{["name", "message", "column", "line", "source"][a]
+                        : [[error. get(i) for i in
+                            ["code", "text", "column_number", "line_number"]]
+                           + ["flake8"]][0][a] for a
+                        in range(5)} for error in errors]} \
+        | {"path": file_path} | \
+        {"status": "failed" if len(errors) > 0 else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [{"errors": [{["name", "message", "column", "line", "source"][a]
+                         :[[error.get(i) for i in
+                            ["code", "text", "column_number", "line_number"]]
+                           + ["flake8"]][0][a] for a in range(5)}
+                        for error in linter_report[b]]}
+            | {"path": b}
+            | {"status": ("failed" if len(linter_report[b]) > 0 else "passed")}
+            for b in linter_report]
