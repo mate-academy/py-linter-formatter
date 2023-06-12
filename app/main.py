7c341fd9642b5732949392ee0ca4d5f741360003
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-06-12 19:32:19 +0100

    py linter formatter

--- app/main.py
+++ app/main.py
@@ -7,21 +7,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{["name", "message", "column", "line", "source"][a]
-                        : [[error. get(i) for i in
-                            ["code", "text", "column_number", "line_number"]]
-                           + ["flake8"]][0][a] for a
-                        in range(5)} for error in errors]} \
+    return {"errors": [format_linter_error(error) for error in errors]} \
         | {"path": file_path} | \
         {"status": "failed" if len(errors) > 0 else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [{["name", "message", "column", "line", "source"][a]
-                         :[[error.get(i) for i in
-                            ["code", "text", "column_number", "line_number"]]
-                           + ["flake8"]][0][a] for a in range(5)}
-                        for error in linter_report[b]]}
-            | {"path": b}
-            | {"status": ("failed" if len(linter_report[b]) > 0 else "passed")}
+    return [format_single_linter_file(b, linter_report[b])
             for b in linter_report]
