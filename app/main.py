e370e345e15a00e5b81c2f2d1a5b7561d77a6007
Andrii Ishchenko <iscenkoa@gmail.com>
2023-06-13 22:03:28 +0300

    Fixed return line

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return \
-        [format_single_linter_file(path, err)
-         for path, err in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
