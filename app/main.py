e3b8ebc8b4a48124dbc2607526abd8a4cede1af3
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-06-17 16:32:27 +0200

    le grand finale

--- app/main.py
+++ app/main.py
@@ -10,40 +10,11 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors
-            ]
-            if errors != [] else [],
+        "errors": [format_linter_error(error) for error in errors] if errors != [] else [],
         "path": file_path,
         "status": "failed" if errors != [] else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors":
-                [
-                    {
-                        "line": error["line_number"],
-                        "column": error["column_number"],
-                        "message": error["text"],
-                        "name": error["code"],
-                        "source": "flake8"
-                    }
-                    for error in errors
-                ]
-                if errors != [] else [],
-            "path": file_path,
-            "status": "failed" if errors != [] else "passed"
-        }
-        for file_path, errors in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
