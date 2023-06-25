0e98aab22a5e6c99a279d5798f9d98dd172d9cbb
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2023-06-26 02:08:44 +0300

    implementation of all functions (format_linter_error, format_single_linter_file, format_linter_report)

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {"line" if key == "line_number" else
+            "column" if key == "column_number" else
+            "message" if key == "text" else
+            "name" if key == "code" else
+            "source": error[key] if key != "filename" else "flake8"
+            for key in error
+            if key == "code" or key == "column_number"
+            or key == "line_number" or key == "text" or key == "filename"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path,
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(file_path=key, errors=linter_report[key])
+            for key in linter_report]
