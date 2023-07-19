1077ee474734491ff7676595be6471e07929cd0a
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-07-20 01:44:30 +0300

    Linter formatter done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(err) for err in errors if err["filename"] == file_path],
+        "path": file_path,
+        "status": "failed" if any(err["filename"] == file_path for err in errors) else "passed"
+    }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_linter_report(linter_report):
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
