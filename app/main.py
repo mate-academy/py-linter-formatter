690bf3ac4c0c12b763d1f6e8a465afb819bd897b
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-07-21 21:14:17 +0300

    Linter formatter done and proposed changes

--- app/main.py
+++ app/main.py
@@ -12,14 +12,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                format_linter_error(err)
-                for err in errors
-                if err["filename"] == file_path
+                format_linter_error(error)
+                for error in errors
             ],
         "path": file_path,
-        "status": "failed"
-        if any(err["filename"] == file_path for err in errors)
-        else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
