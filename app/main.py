c0a604b244002572cf11e27db5d516be850fbc3d
Ruslan <rusipbox@gmail.com>
2023-07-22 22:44:52 +0300

    Test (py-linter-formatter) corrected 22.07.23

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
+    # Format a single linter error into a simple form
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
-
+    # Format linter errors for one file and return a dictionary with formatted error data for the file
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    # Format linter results individually and return a list with formatting results for each file
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
