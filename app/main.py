b942e62e3f3f6b3d86d0e606d3a5c28046074c17
Liftoff <vladislav70707@gmail.com>
2023-07-26 08:18:11 +0200

    Develop_format_report

--- app/main.py
+++ app/main.py
@@ -13,11 +13,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors": [
             format_linter_error(error)
             for error in errors
-            if error["filename"] == file_path
             if error.get("filename") == file_path
         ],
         "path": file_path,
-        "status": "failed" if len(errors) else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
