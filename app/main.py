5da4fdfb73bbd535cfca3eaaeef21d8a3ae444b5
Seniv Volodymyr <01156240@pw.edu.pl>
2023-07-26 20:20:36 +0200

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+         "errors": [
+        #     {
+        #         "line": error["line_number"],
+        #         "column": error["column_number"],
+        #         "message": error["text"],
+        #         "name": error["code"],
+        #         "source": "flake8"
+        #     }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
@@ -27,19 +28,18 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in file_errors
-            ],
-            "path": file_path,
-            "status": "failed" if file_errors else "passed"
-        }
+        format_single_linter_file(file_path, file_errors)
+            # "errors": [
+            #     {
+            #         "line": error["line_number"],
+            #         "column": error["column_number"],
+            #         "message": error["text"],
+            #         "name": error["code"],
+            #         "source": "flake8"
+            #     }
+            #     for error in file_errors
+            # ],
+            # "path": file_path,
+            # "status": "failed" if file_errors else "passed"
         for file_path, file_errors in linter_report.items()
     ]
