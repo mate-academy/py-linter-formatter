b2bf418f4e081bcf9ec98290945202cac3f9553b
Ruslan <rusipbox@gmail.com>
2023-07-29 17:18:21 +0300

    Test (py-linter-formatter) without long lines

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # Format a single linter error into a simple form
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -8,19 +7,17 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # Format linter errors for one file
-    # Return a dictionary with formatted error data for the file
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
-    # Format linter results individually
-    # Return a list with formatting results for each file
     return [
         format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()
+        for file_path, errors in linter_report.items()
     ]
