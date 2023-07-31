d9ed7c1d1e73b89dfa2185550c727f211cec5ad0
Ruslan <rusipbox@gmail.com>
2023-07-31 19:42:17 +0300

    Test (py-linter-formatter) without wraps

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-def format_linter_error(
-        error: dict
-) -> dict:
+def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,21 +8,15 @@ def format_linter_error(
     }
 
 
-def format_single_linter_file(
-        file_path: str,
-        errors: list
-) -> dict:
+def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(error)
-                   for error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
 
 
-def format_linter_report(
-        linter_report: dict
-) -> list:
+def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
