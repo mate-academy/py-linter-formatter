d02e1cb4ba3708365b17504a00aa18aa2b532c84
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-08-03 18:36:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,49 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": errors[0]["line_number"],
-                "column": errors[0]["column_number"],
-                "message": errors[0]["text"],
-                "name": errors[0]["code"],
-                "source": "flake8"
-            },
-            {
-                "line": errors[1]["line_number"],
-                "column": errors[1]["column_number"],
-                "message": errors[1]["text"],
-                "name": errors[1]["code"],
-                "source": "flake8"
-            }
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": [
-                {
-                    "line": item.get("line_number", 0),
-                    "column": item.get("column_number", 0),
-                    "message": item.get("text", ""),
-                    "name": item.get("code", ""),
-                    "source": "flake8"
-                }
-                for item in linter_report.get(file, [])
-            ],
+            "errors": [format_linter_error(item) for item in items],
             "path": file,
-            "status": "failed" if linter_report.get(file) else "passed"
+            "status": "failed" if items else "passed"
         }
-        for file in linter_report.keys()
+        for file, items in linter_report.items()
     ]
