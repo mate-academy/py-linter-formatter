092f9bfdc3e34f3682d96d86c05f344d48b0e0ba
Veronika <veronikaboeva935@gmail.com>
2023-08-04 22:13:49 +0200

    Solution for Litter formatter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
+    return {
+    "line": error["line_number"],
+    "column": error["column_number"],
+    "message": error["text"],
+    "name": error["code"],
+    "source": "flake8"
+}
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"errors": [format_linter_error(error) for error in errors],
+            "status": "failed" if errors else "passed",
+            "path": file_path
+    }
 
+# ведрагувати кожну помилку використовуючи функцію format_linter_error
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()]
+# треба проітеруватися по format_linter_error.items
+# for path, errors in linter_report.items()
+# до errors треба функцію format_single_linter_file
