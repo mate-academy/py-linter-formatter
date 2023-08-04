9daf3f751a1f78b54a9c09a2e2f9a808b7d4755d
Veronika <veronikaboeva935@gmail.com>
2023-08-04 22:20:07 +0200

    Fixed flake8 issues

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
 def format_linter_error(error: dict) -> dict:
     return {
-    "line": error["line_number"],
-    "column": error["column_number"],
-    "message": error["text"],
-    "name": error["code"],
-    "source": "flake8"
-}
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"}
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
             "status": "failed" if errors else "passed",
-            "path": file_path
-    }
-
+            "path": file_path}
 # ведрагувати кожну помилку використовуючи функцію format_linter_error
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(path, errors)
-        for path, errors in linter_report.items()]
+            for path, errors in linter_report.items()]
+
 # треба проітеруватися по format_linter_error.items
 # for path, errors in linter_report.items()
 # до errors треба функцію format_single_linter_file
