096f20c4c29bd46253d78e2e5ff2883ae6b0d78c
Veronika <veronikaboeva935@gmail.com>
2023-08-05 13:54:49 +0200

    Changed formatting mistakes

--- app/main.py
+++ app/main.py
@@ -4,19 +4,24 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"}
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "status": "failed" if errors else "passed",
-            "path": file_path}
-# ведрагувати кожну помилку використовуючи функцію format_linter_error
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "status": "failed" if errors else "passed",
+        "path": file_path
+    }
+# відрагувати кожну помилку використовуючи функцію format_linter_error
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors)
-            for path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
 
 # треба проітеруватися по format_linter_error.items
 # for path, errors in linter_report.items()
