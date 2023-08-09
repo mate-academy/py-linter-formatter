21b6cfc3fba75de5ee9a29efa9cc9440bc8f3085
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2023-08-09 20:02:23 +0300

    Improve codestyle

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            format_linter_error(error) for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed",
     }
@@ -20,6 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(path, linter_report[path])
-        for path in linter_report
+        format_single_linter_file(file_path, error_list)
+        for file_path, error_list in linter_report.items()
     ]
