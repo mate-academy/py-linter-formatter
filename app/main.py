c5970bbca4d090c5e138c409eb2989e709378812
Nikolay Krasulin <krasulinphotograph@example.com>
2023-08-09 09:23:22 +0300

    edit third function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error.pop("line_number"), "column": error.pop("column_number"), "name": error.pop("code"),
-            "message": error.pop("text"), "source": "flake8"
+        "line": error.pop("line_number"),
+        "column": error.pop("column_number"),
+        "name": error.pop("code"),
+        "message": error.pop("text"),
+        "source": "flake8"
     }
 
 
@@ -18,19 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error.pop("line_number"),
-                    "column": error.pop("column_number"),
-                    "message": error.pop("text"),
-                    "name": error.pop("code"),
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
