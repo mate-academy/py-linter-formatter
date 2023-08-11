1689f1892f4b7992650cf5583b2837496c99e780
Depaton <you@example.com>
2023-08-11 18:10:44 +0300

    New solution with changes.

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(num) for num in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
@@ -18,6 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(key, linter_report[key])
-        for key in linter_report
+        format_single_linter_file(key, values)
+        for key, values in linter_report.items()
     ]
