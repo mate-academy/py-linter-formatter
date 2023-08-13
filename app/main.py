3152f8e1b7e81dda901e9f33ead24928113fb76d
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-08-13 23:29:08 +0300

    'Solution_py-linter-formatter_vvg'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return {
+        "errors": [
+            format_linter_error(err) for err in errors
+        ],
+        "path": file_path,
+        "status": "passed" if errors == [] else "failed"
+    }
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return [
+        format_single_linter_file(linter[0], linter[1])
+        for linter in list(linter_report.items())
+    ]
     pass
