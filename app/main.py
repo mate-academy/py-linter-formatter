5ced924809bc971f3b53e48f83154ed8d7cb0be0
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-08-14 18:26:54 +0300

    'annotations'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 def format_linter_error(error: dict) -> dict:
+    # formats a single error:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"
+        "source": "flake8"  # add key "source" : value = "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+    #  formats all errors
     return {
         "errors": [
             format_linter_error(err) for err in errors
@@ -19,6 +21,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
+    # formats all errors for all report files:
     return [
         format_single_linter_file(linter[0], linter[1])
         for linter in list(linter_report.items())
