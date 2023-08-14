7b7d83c6172abdf32a334afcd80849c7f467e1a0
TROT <seotrotpol@gmail.com>
2023-08-14 19:04:46 +0300

    'bug fixed'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,33 @@
+# def format_linter_error(error: dict) -> dict:
+#
+#     return {
+#         "line": error["line_number"],
+#         "column": error["column_number"],
+#         "message": error["text"],
+#         "name": error["code"],
+#         "source": "flake8"
+#     }
+#
+#
+# def format_single_linter_file(file_path: str, errors: list) -> dict:
+#
+#     return {
+#         "errors": [format_linter_error(error) for error in errors],
+#         "path": file_path,
+#         "status": "failed" if errors else "passed"
+#     }
+#
+#
+# def format_linter_report(linter_report: dict) -> list:
+#
+#     return [
+#         {
+#             "errors": [format_linter_error(error) for error in errors],
+#             "path": file_path,
+#             "status": "failed" if errors else "passed"
+#         }
+#         for file_path, errors in linter_report.items()
+#     ]
 def format_linter_error(error: dict) -> dict:
 
     return {
@@ -21,10 +51,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
 
     return [
-        {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
