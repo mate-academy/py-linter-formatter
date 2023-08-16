9e6db5651670ca3e725c168d8d4b147d64ade3d0
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2023-08-16 16:15:58 +0300

    corrected file_path, errors

--- app/main.py
+++ app/main.py
@@ -19,5 +19,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
-        for key, value in linter_report.items()
+        for file_path, errors in linter_report.items()
     ]
