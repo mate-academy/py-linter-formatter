ed900ae0cf8135e3dd0d657f73795c3a1ac53355
Yaroslav Yarovyi <yarovoyyaroslav21@gmail.com>
2023-08-16 11:08:34 +0300

    Delete empty lines

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +9,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {
         "path": file_path,
         "status": "failed" if errors else "passed",
