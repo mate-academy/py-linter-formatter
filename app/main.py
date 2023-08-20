b61d34ab3e3211074da8a2f97bc0f2d191ea8ab3
Grigoriy Gesiod <g.gesiod@gmail.com>
2023-08-21 02:17:52 +0300

    Wrote 2/3 functions

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"error": [{"line": error["line_number"],
+            "column": error["line_number"],
+                       "message": error["column_number"],
+                       "code": error["line_number"],
+                       "source": "flake8"} for error in errors],
+            "path": file_path,
+            "status": ("failed" if len(errors) > 0 else "passed")}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return []
