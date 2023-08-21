ad82e468e617b1ec1614acf4eb39afabe62b4d8f
Grigoriy Gesiod <g.gesiod@gmail.com>
2023-08-21 03:49:26 +0300

    Wrote 3/3 functions

--- app/main.py
+++ app/main.py
@@ -7,14 +7,21 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"error": [{"line": error["line_number"],
-            "column": error["line_number"],
-                       "message": error["column_number"],
-                       "code": error["line_number"],
-                       "source": "flake8"} for error in errors],
+    return {"errors": [{"line": error["line_number"],
+                        "column": error["column_number"],
+                       "message": error["text"], "name": error["code"],
+                        "source": "flake8"} for error in errors],
             "path": file_path,
             "status": ("failed" if len(errors) > 0 else "passed")}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return []
+    return [{"errors": [{"line": lin_err["line_number"],
+                         "column": lin_err["column_number"],
+                         "message": lin_err["text"],
+                         "name": lin_err["code"],
+                         "source": "flake8"
+                         } for lin_err in lin_value],
+             "path": lin_key,
+             "status": ("failed" if len(lin_value) > 0 else "passed")}
+            for lin_key, lin_value in linter_report.items()]
