34c6f5187789691460f2b0682ffc8849ea22561f
Poplinskiy Dmytro <super.shepard2@gmail.com>
2023-08-22 18:27:29 +0300

    resolved review tips, use `format_linter_error` and `format_single_linter_file` functions

--- app/main.py
+++ app/main.py
@@ -11,35 +11,16 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
-        "status": "failed"
+        "status": "passed" if len(errors) == 0 else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in report
-            ],
-            "path": file_name,
-            "status": "passed" if len(report) == 0 else "failed"
-        }
+        format_single_linter_file(file_name, report)
         for file_name, report in linter_report.items()
     ]
