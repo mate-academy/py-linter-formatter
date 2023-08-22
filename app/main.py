f1cc35bbdf55b0db669845a0f96e379d6c5a1096
Mykola <kradu2001@gmail.com>
2023-08-22 04:59:15 +0300

    Fixed all duplicate code, simplified if-condition

--- app/main.py
+++ app/main.py
@@ -24,5 +24,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, list_errors)
-            for file_path, list_errors in linter_report.items()]
+    return [
+        format_single_linter_file(file_path, list_errors)
+        for file_path, list_errors in linter_report.items()
+    ]
