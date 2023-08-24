7fe2050cc5d9ba9a0b5bc7df318863e031977e33
Oleksandr Kolesnikov <35701518+abeemaster@users.noreply.github.com>
2023-08-24 14:42:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,47 +9,18 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    list_in_dict = []
-    for elem in errors:
-        formatted_error = {
-            "line": elem["line_number"],
-            "column": elem["column_number"],
-            "message": elem["text"],
-            "name": elem["code"],
-            "source": "flake8"
-        }
-        list_in_dict.append(formatted_error)
-
-    status = "failed" if list_in_dict else "passed"
-
     return {
-        "errors": list_in_dict,
+        "errors": [format_linter_error(error=error) for error in errors],
         "path": file_path,
-        "status": status
+        "status": "failed" if "errors" else "passed"
     }
 
-
 def format_linter_report(linter_report: dict) -> list:
-    formatted_reports = []
-    for file_path, errors in linter_report.items():
-        formatted_errors = []
-        for elem in errors:
-            formatted_error = {
-                "line": elem.get("line_number"),
-                "column": elem.get("column_number"),
-                "message": elem.get("text"),
-                "name": elem.get("code"),
-                "source": "flake8"
-            }
-            formatted_errors.append(formatted_error)
-        formatted_reports.append({
-            "errors": formatted_errors,
-            "path": file_path
-        })
-    for report in formatted_reports:
-        if report["errors"]:
-            report["status"] = "failed"
-        else:
-            report["status"] = "passed"
-
-    return formatted_reports
+    return [
+        {
+            "errors": format_single_linter_file(file_path="./source_code_2.py", errors=errors),
+            "path": file_path,
+            "status": "failed" if "errors" else "passed"
+        }
+        for file_path, errors in linter_report.items()
+    ]
