9d901ef8accb01e2b1a27931906c5a3829f118c7
Grigoriy Gesiod <g.gesiod@gmail.com>
2023-08-24 10:28:32 +0300

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"}
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error)
-                   if error else "" for error in errors],
+                   for error in errors],
         "path": file_path,
         "status": ("failed" if errors else "passed")}
 
