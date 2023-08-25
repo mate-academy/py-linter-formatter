0191df6cff6cb8100dae17fbfa5edcf306db7f12
Oleksandr <a.beemaster@gmail.com>
2023-08-25 21:24:39 +0300

    New working version

--- app/main.py
+++ app/main.py
@@ -18,11 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": format_single_linter_file(file_path=file_path,
-                                                errors=errors)["errors"],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
