39435a906a59dd1e34104c87c47ccbb4a00fe2bd
Nataliia Horishna <gorishnatalia@gmail.com>
2023-08-30 14:38:34 +0300

    Rename items

--- app/main.py
+++ app/main.py
@@ -13,6 +13,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        format_single_linter_file(key, value)
-        for key, value in linter_report.items()]
+    return [format_single_linter_file(path, erorrs)
+            for path, erorrs in linter_report.items()]
