d2b6a9c5aea64cc17189a83463aa9836685a577a
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2023-09-03 14:45:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,46 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+            }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            {
+                "line": err["line_number"],
+                "column": err["column_number"],
+                "message": err["text"],
+                "name": err["code"],
+                "source": "flake8"
+            }
+            for err in errors
+            if err["filename"] == file_path
+        ],
+        "path": file_path,
+        "status": "passed" if not errors else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in errors
+            ],
+            "path": path,
+            "status": "passed" if not errors else "failed"
+        }
+        for path, errors in linter_report.items()
+    ]
