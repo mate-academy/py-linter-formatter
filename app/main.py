62991c1d86068212dd2275747dfc2c8cef6baa64
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2023-09-06 10:56:39 +0300

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,28 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-            "line": error["line_number"],
+    return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
-            "source": "flake8"
-    }
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(err)
-                   for err in errors if err["filename"] == file_path],
+        "errors": [format_linter_error(err) for err in errors],
         "path": file_path,
-        "status": "passed" if not errors else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": path,
-            "status": "passed" if not errors else "failed"
-        }
+        format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
     ]
