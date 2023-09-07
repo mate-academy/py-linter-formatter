14fcd999eb744021691415c98da92b101a5d1720
Stepan Shevchuk <135757419+StepanShevchuk@users.noreply.github.com>
2023-09-07 19:14:49 +0100

    Update app/main.py
    Co-authored-by: Serhii <58142395+Grayder0152@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -15,5 +15,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(error, linter_report[error])
-            for error in linter_report]
+    use `.items()` in this case
