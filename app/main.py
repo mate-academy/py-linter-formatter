8131086bde9e7dc72bcf521690690c9969974912
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-07 19:47:02 +0100

    py linter formatter

--- app/main.py
+++ app/main.py
@@ -13,4 +13,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    use `.items()` in this case
+    return [format_single_linter_file(error[0], error[1])
+            for error in linter_report.items()]
