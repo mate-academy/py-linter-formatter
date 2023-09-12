602f38c16f22a4fa19ac5b0463c27a6e46822562
Mykola Mazhula <mykm3ua@gmail.com>
2023-09-12 17:44:23 +0200

    Solution got rid of repeating code

--- app/main.py
+++ app/main.py
@@ -9,22 +9,12 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [
-        {"line": error["line_number"],
-         "column": error["column_number"],
-         "message": error["text"],
-         "name": error["code"],
-         "source": "flake8"} for error in errors],
+        format_linter_error(error) for error in errors],
         "path": file_path,
         "status": ("failed" if errors else "passed")}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [
-        {"line": error["line_number"],
-         "column": error["column_number"],
-         "message": error["text"],
-         "name": error["code"],
-         "source": "flake8"} for error in linter_report.get(file_report)],
-        "path": file_report,
-        "status": ("failed" if linter_report.get(file_report) else "passed")}
-        for file_report in linter_report]
+    return [format_single_linter_file(file_report,
+                                      linter_report.get(file_report))
+            for file_report in linter_report]
