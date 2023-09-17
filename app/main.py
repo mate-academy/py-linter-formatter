75b0c16bc99df8c82d3054a4b5671df2830273c6
Vlad Nimets <hermesvlad@gmail.com>
2023-09-17 20:03:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,11 +7,14 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
         "status": "failed" if errors else "passed",
-        "errors":[format_linter_error(error) for error in errors]}
+        "errors": [format_linter_error(error) for error in errors]}
+
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
