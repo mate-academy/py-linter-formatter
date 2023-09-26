8b5eb3b7080703f9e717363aaa5426010a20a81c
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-09-26 16:51:54 +0300

    use .items() and update var names

--- app/main.py
+++ app/main.py
@@ -18,5 +18,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(i, linter_report[i]) for i in linter_report
+        format_single_linter_file(file_path, errors) for file_path, errors  in linter_report.items()
     ]
