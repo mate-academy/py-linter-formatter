22fb30dad1f802c6be9c5f5248405253ee2f948e
Dmytro Petrykiv <petrykiv.dmytro19@gmail.com>
2023-09-27 08:18:20 +0300

    Refactoring format_linter_report func.

--- app/main.py
+++ app/main.py
@@ -17,5 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, linter_report[path])
-            for path in linter_report.keys()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
