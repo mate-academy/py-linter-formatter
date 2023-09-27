6f75514bf8ad29565d75c02dc73dae6fd131d5bc
sakhaline <alexandra.ivva@gmail.com>
2023-09-27 08:52:54 +0300

    Remove else statement

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors": [
             format_linter_error(error)
             for error in errors
-        ] if errors else [],
+        ],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
