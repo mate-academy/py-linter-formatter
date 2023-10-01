4805df37124ad2553c76d42e5f3a474586e73838
Bohdan Tsapenko <earldoomy@gmail.com>
2023-10-01 15:40:54 +0400

    Have not maintained DRY principle in my last commit indeed. I have refactored the code now and though it does not follow your suggestion to the T - should still be good

--- app/main.py
+++ app/main.py
@@ -10,34 +10,16 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-            for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed"
+        "status": "failed" if [format_linter_error(error) for error in errors] else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8",
-                }
-                for error in linter_report[file_path]
-            ],
+            "errors": [format_linter_error(error) for error in linter_report[file_path]],
             "path": file_path,
             "status": "failed" if linter_report[file_path] else "passed",
         }
