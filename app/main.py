0369357ffe20c9c9fa52b38a5333188a8c44ea93
Bohdan Tsapenko <earldoomy@gmail.com>
2023-10-01 15:45:02 +0400

    Refactored the code to follow the DRY principle

--- app/main.py
+++ app/main.py
@@ -12,14 +12,16 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if [format_linter_error(error) for error in errors] else "passed"
+        "status": "failed" if [format_linter_error(error) for error in errors]
+        else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": [format_linter_error(error) for error in linter_report[file_path]],
+            "errors": [format_linter_error(error)
+                       for error in linter_report[file_path]],
             "path": file_path,
             "status": "failed" if linter_report[file_path] else "passed",
         }
