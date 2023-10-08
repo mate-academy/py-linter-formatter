cd06afd4a83a7571351f01bc2558a30335de6ede
Yuliia Dudych <129203133+CrazyHilly@users.noreply.github.com>
2023-10-08 13:09:45 +0300

    Fix formatting
    Co-authored-by: ArsenPidhoretskyi <65166203+ArsenPidhoretskyi@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -19,6 +19,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_name, errors)
-        for file_name, errors
-        in linter_report.items()
+        for file_name, errors in linter_report.items()
     ]
