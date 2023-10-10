817613a2047554bcfaeb6e2717886027657655c5
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2023-10-10 19:57:36 +0300

    Error list added to variable

--- app/main.py
+++ app/main.py
@@ -1,26 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"
-    }
+    return dict(
+        line=error["line_number"],
+        column=error["column_number"],
+        message=error["text"],
+        name=error["code"],
+        source="flake8"
+    )
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {
-        "errors": [format_linter_error(error)
-                   for error in errors
-                   if error["filename"] == file_path
-                   ],
-        "path": file_path,
-        "status":
-        "failed" if [format_linter_error(error)
-                     for error in errors
-                     if error["filename"] == file_path
-                     ] else "passed"
-    }
+    return dict(
+        errors=[format_linter_error(error)
+                for error in errors
+                if error["filename"] == file_path],
+        path=file_path,
+        status="failed" if errors else "passed"
+    )
 
 
 def format_linter_report(linter_report: dict) -> list:
