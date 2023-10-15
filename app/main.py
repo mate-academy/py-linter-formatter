6234c7cc342c8e4c07391b9524e55aaab98a8583
Felix Poslushnyi <felixp4@gmail.com>
2023-10-15 22:30:24 +0300

    Reformat solution

--- app/main.py
+++ app/main.py
@@ -1,25 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line" if key == "line_number" else
-            "column" if key == "column_number" else
-            "message" if key == "text" else
-            "name" if key == "code" else
-            "source": "flake8" if key == "physical_line" else
-            val for key, val in (error.items())}
+    return {
+        {
+            "code": "name",
+            "text": "message",
+            "line_number": "line",
+            "column_number": "column",
+            "filename": "source"
+        }[key]: ("flake8" if key == "filename" else val)
+        for (key, val) in error.items()
+        if key != "physical_line"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
-            "status": "passed" if len(errors) == 0 else "failed"}
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": linter_report[key],
-            "path": key,
-            "status": "passed" if (len(linter_report[key]) == 0) else "failed"
-        } if item == [] else
-        format_single_linter_file(key, linter_report[key])
-        for (key, item) in linter_report.items()
-    ]
+    return [format_single_linter_file(key, linter_report[key])
+            for (key, item) in linter_report.items()
+            ]
