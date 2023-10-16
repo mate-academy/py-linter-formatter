aa82e7d1d9c7966fa42403e2cb8da6131f1ae587
Felix Poslushnyi <felixp4@gmail.com>
2023-10-16 20:10:33 +0300

    more descriptive names

--- app/main.py
+++ app/main.py
@@ -6,18 +6,17 @@ def format_linter_error(error: dict) -> dict:
             "line_number": "line",
             "column_number": "column",
             "filename": "source"
-        }[key]: ("flake8" if key == "filename" else val)
-        for (key, val) in error.items()
-        if key != "physical_line"}
+        }[signature]: ("flake8" if signature == "filename" else sense)
+        for signature, sense in error.items()
+        if signature != "physical_line"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path,
-            "status": "passed" if errors == [] else "failed"}
+            "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, linter_report[key])
-            for (key, item) in linter_report.items()
-            ]
+    return [format_single_linter_file(signature, linter_report[signature])
+            for signature in linter_report]
