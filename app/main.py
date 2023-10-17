67a2c068504ac22fddaed3539bd4d1d2a676157f
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-10-17 15:21:49 +0300

    все зробив один тест не проходить

--- app/main.py
+++ app/main.py
@@ -17,5 +17,3 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(key, error) for key, error in linter_report.items()]
-
-
