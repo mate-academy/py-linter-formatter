2dff36990d1afe7c66a694d94533fa8ac0e154ee
Felix Poslushnyi <felixp4@gmail.com>
2023-10-18 06:06:55 +0300

    replace keys with list and loop error items

--- app/main.py
+++ app/main.py
@@ -1,22 +1,21 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        {
-            "code": "name",
-            "text": "message",
-            "line_number": "line",
-            "column_number": "column",
-            "filename": "source"
-        }[signature]: ("flake8" if signature == "filename" else sense)
-        for signature, sense in error.items()
-        if signature != "physical_line"}
+        ["name", "source", "line", "column", "message"][i]:
+        ("flake8" if key == "filename" else error[key])
+        for i, key in enumerate(error) if key != "physical_line"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(signature, linter_report[signature])
-            for signature in linter_report]
+    return [
+        format_single_linter_file(signature, linter_report[signature])
+        for signature in linter_report
+    ]
