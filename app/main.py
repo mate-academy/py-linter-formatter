4c267231c4a4dd7ffd0316645e089c9de59eb5d9
Mikola Rozhansky <bymikola@gmail.com>
2023-10-20 00:14:19 +0300

    format_linter_error

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ def format_linter_error(error: dict) -> dict:
     # write your code here
     pass
     return {
-        "line" : error["line_number"]
-        "column" : error["column_number"]
-        "message" : error["text"]
-        "name" : error["code"]
-        "source" : "flake8"
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
 
     }
 
