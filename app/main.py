d276c60f9a35abdb42208791682a2f2bccc09403
Mikola Rozhansky <bymikola@gmail.com>
2023-10-23 00:16:12 +0300

    All ok!

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    # pass
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -19,27 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 
-
-
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
     return [
-        {
-            "errors": list(),
-            "path": list(linter_report.keys())[0],
-            "status": "passed"
-
-        },
-        # linter_report.get("./source_code_2.py")
-        # format_single_linter_file(list(linter_report.keys())[0], data)
-        # никак не могу понять как сделать правильно !!!!!
-        [format_single_linter_file(list(linter_report.keys())[i], data) for i, data in enumerate(list(linter_report.values())) if i>0]
-        # format_single_linter_file(list(linter_report.keys())[0], list(linter_report.values())[2])
-        # for i, data in enumerate(list(linter_report.values())):
-        #     if i > 0:
-        #         format_single_linter_file(list(linter_report.keys())[i], data)
-
-
-
-
+        format_single_linter_file(i, data) for i, data in linter_report.items()
     ]
