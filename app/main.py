9fca9fabb5aaca2fee7f341cc5bb76276b9c05f4
Mikola Rozhansky <bymikola@gmail.com>
2023-10-22 21:51:37 +0300

    format_linter_error

--- app/main.py
+++ app/main.py
@@ -11,10 +11,24 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+
+        "errors": [format_linter_error(num) for num in errors],
+        "path": file_path,
+        "status": "failed" if len(errors) else "passed"
+
+
+    }
+
+    #     "errors": {
+    #         "line": data["line_number"],
+    #         "column": data["column_number"],
+    #         "message": data["text"],
+    #         "name": data["code"],
+    #         "source": "flake8"
+    #         for data in errors]
+    # },
 
 
 def format_linter_report(linter_report: dict) -> list:
