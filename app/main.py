f9300b5dcf2700888f6221facdc0963a6679890f
Tymur Shmorhun <shmorguntimur@gmail.com>
2023-10-24 21:30:56 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -12,26 +12,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
         "path": file_path,
-        "status": "failed" if [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-            for error in errors
-        ] else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
