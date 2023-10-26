0d429f8f4d6f47dda99cbaef5242cf58c4c875d0
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-10-26 13:07:55 +0200

    try to delete strange whitespace

--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path, errors) 
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
     
