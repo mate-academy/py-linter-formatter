c6784e97bcb412107edea5f779ebc8d2e044be3d
Oleksii Mileiko <alexexile1987@gmail.com>
2023-10-28 15:01:25 +0300

    remove all pass

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def format_linter_error(error: dict) -> dict:
         "message": error["text"],
         "name": error["code"],
         "source": "flake8"}
-    pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -15,12 +14,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
             "path": file_path, "status": "failed" if errors else "passed"
             }
 
-    pass
-
 
 def format_linter_report(linter_report: dict) -> list:
 
     return [format_single_linter_file(file_path, errors)
             for file_path, errors in linter_report.items()]
-
-    pass
