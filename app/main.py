7066894366602bb0fb76b1cc5cf10da1997f656e
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-10-30 22:01:40 +0200

    Changes according to request with arguments on a new line

--- app/main.py
+++ app/main.py
@@ -4,13 +4,16 @@ def format_linter_error(error: dict) -> dict:
         "column": error.get("column_number"),
         "message": error.get("text"),
         "name": error.get("code"),
-        "source": "flake8"}
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "passed" if errors == [] else "failed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if errors == [] else "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
