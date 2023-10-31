6735c1357119b7ed8461b725a52b97d3f9bb5767
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2023-10-31 12:54:30 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -21,13 +21,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                format_linter_error(error)
-                for error in linter_report[path]
-            ],
-            "path": path,
-            "status": "failed" if len(linter_report[path]) else "passed"
-        }
-        for path in linter_report
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
     ]
