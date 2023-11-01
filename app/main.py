80aada297014ee30cf49d9fce46e8ad08664f925
Kyryl Zhelizko <kyryl.zhelizko@gmail.com>
2023-11-01 16:09:53 -0700

    fix annotation

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
