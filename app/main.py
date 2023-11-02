ea6cddf0c71ac862d3f4851cbb4ecec2833ddc27
Alisa <alisasklrv@gmail.com>
2023-11-02 18:59:46 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    return ({"line" if key == "line_number"
-            else "column" if key == "column_number"
-            else "message" if key == "text"
-            else "name" if key == "code" else key: value
-            for key, value in error.items()
-            if key not in ("filename", "physical_line")}
-            | {"source": "flake8"})
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return ({"errors": [format_linter_error(error) for error in errors]}
-            | {"path": file_path}
-            | {"status": "passed" if len(errors) == 0 else "failed"})
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path,
+            "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(path, errors)
+            for path, errors in linter_report.items()]
