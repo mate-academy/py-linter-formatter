eb927c636d690162e19f832359b1e9add965fdc7
Kolya Ostrovka <mykolaostrovka27@gmail.com>
2023-11-06 16:10:09 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,57 +1,3 @@
-def format_single_linter_file_2(file_path: str, errors: list) -> dict:
-    res_dict_2 = {}
-    result = []
-
-    for error in errors:
-        formatted_error = {
-            "line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-        }
-
-        result.append(formatted_error)
-
-        res_dict_2["errors"] = result
-        res_dict_2["path"] = error["filename"]
-
-    if errors == []:
-        res_dict_2["status"] = "passed"
-    else:
-        res_dict_2["status"] = "failed"
-    return res_dict_2
-
-
-def format_linter_report_2(linter_report: dict) -> list:
-    result_list = []
-    for key, value in linter_report.items():
-        if value == []:
-            result_dict = {
-                "errors": [],
-                "path": key,
-                "status": "passed"
-            }
-        else:
-            error = []
-            for values in value:
-                formatted_error = {
-                    "line": values["line_number"],
-                    "column": values["column_number"],
-                    "message": values["text"],
-                    "name": values["code"],
-                    "source": "flake8"
-                }
-                error.append(formatted_error)
-            result_dict = {
-                "errors": error,
-                "path": key,
-                "status": "failed"
-            }
-        result_list.append(result_dict)
-    return result_list
-
-
 def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
@@ -63,8 +9,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return format_single_linter_file_2(file_path, errors)
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return format_linter_report_2(linter_report)
+    return [
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
+    ]
