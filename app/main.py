adf5b630b24c117f64e1b767f7ccbbb20d5d7af5
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-11-21 22:40:50 +0100

    create def linter_error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {("name" if key == "code"
+             else "line" if key == "line_number"
+             else "column" if key == "column_number"
+             else "message" if key == "text"
+             else "source"): ("flake8" if str(value).startswith("./s") else value)
+            for key, value in (dict(list(error.items())[2:5] + list(error.items())[:2])).items()
+            }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
