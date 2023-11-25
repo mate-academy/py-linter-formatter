ae3bb25c389d9b7783d16650a2b96de4dd104f77
PythonZem <pythonzem@gmail.com>
2023-11-25 20:10:48 +0200

    fix flake8 1

--- app/main.py
+++ app/main.py
@@ -9,10 +9,12 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [] if len(errors) == 0 else [format_linter_error(error=error) for error in errors],
+    return {"errors": [] if len(errors) == 0
+            else [format_linter_error(error=error) for error in errors],
             "path": file_path,
             "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path=path, errors=errors) for path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path=path, errors=errors)
+            for path, errors in linter_report.items()]
