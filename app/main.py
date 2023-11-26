6c0167922c73b0764015d4d8288a92bc48658ebe
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-11-26 20:55:22 +0100

    simplify code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
-def format_linter_error(err: dict) -> dict:
+def format_linter_error(error: dict) -> dict:
     return {
-        "line": err["line_number"],
-        "column": err["column_number"],
-        "message": err["text"],
-        "name": err["code"],
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(val) for val in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(element, val_of_element)
+            for element, val_of_element in linter_report.items()]
