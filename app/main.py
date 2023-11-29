1d33095232136493ffcc3a7779a504ce8b79c4c1
Andrii Zubchenko <dissom1987@gmail.com>
2023-11-29 18:38:18 +0200

    Changed

--- app/main.py
+++ app/main.py
@@ -10,11 +10,12 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    
+
     return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path, 
+            "path": file_path,
             "status": "passed" if errors == [] else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key, value  in linter_report.items()]
+    return [format_single_linter_file(key, value)
+            for key, value in linter_report.items()]
