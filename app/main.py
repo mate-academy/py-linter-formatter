d1e70ce5dd92e29086b841847351191e619bca62
Max <bidun.max@gmail.com>
2023-11-30 23:12:03 +0200

    Solution error correction

--- app/main.py
+++ app/main.py
@@ -16,4 +16,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
 
-    return [format_single_linter_file(file_path, error) for file_path, error in linter_report.items()]
+    return [format_single_linter_file(file_path, error)
+            for file_path, error in linter_report.items()]
