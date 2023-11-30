f94f9fd467335cf9b15cf5be6e704b1c3da0e7f9
Viktar Chaunavy <paca.vaca.pachvara@gmail.com>
2023-11-30 22:51:55 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,13 +10,10 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [{
-            "line": every["line_number"],
-            "column": every["column_number"],
-            "message": every["text"],
-            "name": every["code"],
-            "source": "flake8"
-        } for every in errors if every["filename"] == file_path],
+        "errors": [
+            format_linter_error(error=every) for every in errors
+            if every["filename"] == file_path
+        ],
         "path": file_path,
         "status": "failed" if len(errors) > 0 else "passed"
     }
@@ -24,15 +21,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [{
-            "line": linter_report[file_error][key_error]["line_number"],
-            "column": linter_report[file_error][key_error]["column_number"],
-            "message": linter_report[file_error][key_error]["text"],
-            "name": linter_report[file_error][key_error]["code"],
-            "source": "flake8"}
-            for key_error, key in enumerate(linter_report[file_error])],
-            "path": file_error,
-            "status": "passed"
-            if len(linter_report[file_error]) == 0 else "failed"}
+        format_single_linter_file(file_path=file_error,
+                                  errors=linter_report[file_error])
         for file_error in linter_report
     ]
