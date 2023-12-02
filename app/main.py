aaed47a7e80e8741c58a480a7f14595e2a5afe65
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-12-02 12:57:20 +0200

    task_3 solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,61 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    if len(error) == 0:
+        return {}
+    formatted_error = {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+    return formatted_error
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    errors_list = []
+    status = 'failed' if len(errors) != 0 else 'passed'
+    if status == 'failed':
+        for error in errors:
+            formatted_error = {
+
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            }
+            errors_list.append(formatted_error)
+    result_dict = {'errors': errors_list,
+                   'path': file_path,
+                   'status': status}
+    return result_dict
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    result = []
+    file_names = list(linter_report.keys())
+    status_list = []
+    for file_name in file_names:
+        status_list.append('passed' if len(linter_report[file_name]) == 0 else 'failed')
+
+    for value in range(len(status_list)):
+        sub_list = []
+        if status_list[value] == 'failed':
+            errors_list = linter_report[file_names[value]]
+            for error in errors_list:
+                formatted_error = {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                sub_list.append(formatted_error)
+
+        sub_dict = {'errors' : sub_list,
+                    'path' : file_names[value],
+                    'status' : status_list[value]}
+
+        result.append(sub_dict)
+
+    return result
