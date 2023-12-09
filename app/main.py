139422c379e90d3c3c2db93e9283bf5c5d331af8
rtsadm <98526243+rtsadm@users.noreply.github.com>
2023-12-09 19:03:39 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {"line": error["line_number"], "column": error["column_number"],
+            "message": error["text"], "name": error["code"], "source": "flake8"
+            }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {"path": file_path,
+            "status": "passed" if len(errors) == 0 else "failed",
+            "errors": [format_linter_error(error) for error in errors]
+            }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(pth, er) for pth, er in linter_report.items()
+    ]
