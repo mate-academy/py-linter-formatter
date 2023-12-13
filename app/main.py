51475257717d9e09d7a5b97a38dbf035c6eb713b
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-13 12:35:51 +0200

    solve format_linter_error() in app/main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
-def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
     pass
@@ -11,3 +6,14 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
     pass
+
+
+def format_linter_error(error: dict) -> dict:
+    # write your code here
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+    }
