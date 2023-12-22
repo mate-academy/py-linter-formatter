4d56edaf8f6686173614a044fe2b1538152feefe
PythonZem <pythonzem@gmail.com>
2023-12-22 22:26:25 +0200

    fix my style for flake8

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def format_linter_error(error: dict) -> dict:
-    return {
-            "line": error["line_number"],
+    return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
             "name": error["code"],
@@ -9,15 +8,13 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {
-            "errors": [format_linter_error(error=error) for error in errors],
+    return {"errors": [format_linter_error(error=error) for error in errors],
             "path": file_path,
             "status": "passed" if errors == [] else "failed"
             }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-            format_single_linter_file(file_path=path, errors=errors)
+    return [format_single_linter_file(file_path=path, errors=errors)
             for path, errors in linter_report.items()
             ]
