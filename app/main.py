5fbd875320191c618265980b46310f3c73938140
ladmael <don.sosiska339@gmail.com>
2023-12-23 15:36:29 +0200

    flake fix1

--- app/main.py
+++ app/main.py
@@ -18,5 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(errors, linter_report.get(errors)) for errors in linter_report
+        format_single_linter_file(errors, linter_report.get(errors))
+        for errors in linter_report
     ]
