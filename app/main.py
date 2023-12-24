daa1a3043a7c57c3d791656b47ca30e963c57477
kabust <tulyulyuk91@gmail.com>
2023-12-24 12:36:37 +0100

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
-error_vars = {"line": "line_number",
-              "column": "column_number",
-              "message": "text",
-              "name": "code",
-              "source": "flake8"}
-
-
 def format_linter_error(error: dict) -> dict:
-    return {k: (error[v] if v in error.keys() else v)
-            for k, v in error_vars.items()}
+    return {
+        k: (error[v] if v in error.keys() else v)
+        for k, v in {"line": "line_number",
+                     "column": "column_number",
+                     "message": "text",
+                     "name": "code",
+                     "source": "flake8"}.items()
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed" if errors else "passed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path=path,
-                                      errors=linter_report[path])
-            for path in linter_report.keys()]
+    return [
+        format_single_linter_file(file_path=file_path, errors=errors)
+        for file_path, errors in linter_report.items()
+    ]
