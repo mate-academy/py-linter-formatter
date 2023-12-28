648db2014adc2616b4e742baaf878cdcdcdaf4b4
ScienceUA <o@upman.group>
2023-12-28 15:29:42 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,12 +9,10 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    formatted_errors = [format_linter_error(error) for error in errors]
-    status = "failed" if errors else "passed"
     return {
-        "errors": formatted_errors,
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": status,
+        "status": "failed" if errors else "passed",
     }
 
 
