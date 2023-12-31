d05529fdc625c157be95eac70360ad123946332f
Mykola Moskaliuk <154981650+diexpron@users.noreply.github.com>
2023-12-31 13:21:48 +0200

    made function 2 without comperhension

--- app/main.py
+++ app/main.py
@@ -9,21 +9,39 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    d = {}
+    m = []
+    for i in range(len(errors)):
+        m.append(format_linter_error(errors[i]))
+    d["errors"] = m
+    d["path"] = file_path
+    d["status"] = " ".join(["failed" if len(m) > 0 else "passed"])
+    return d
 
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
     pass
 
-error = {
-    "code": "E501",
-    "filename": "./source_code_2.py",
-    "line_number": 18,
-    "column_number": 80,
-    "text": "line too long (99 > 79 characters)",
-    "physical_line": '    return f"I like to filter, rounding, doubling, '
-    "store and decorate numbers: {', '.join(items)}!\"",
-}
-print(format_linter_error(error))
+errors = [
+    {
+        "code": "E501",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 80,
+         "text": "line too long (99 > 79 characters)",
+        "physical_line": '    return f"I like to filter, rounding, doubling, ' 
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+    {
+        "code": "W292",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 100,
+        "text": "no newline at end of file",
+        "physical_line": '    return f"I like to filter, rounding, doubling, '
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+]
+
+print(format_single_linter_file("./source_code_2.py", errors))
