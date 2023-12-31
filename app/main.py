717322fb6647530203deb63c4b23d4307e775148
Mykola Moskaliuk <154981650+diexpron@users.noreply.github.com>
2023-12-31 13:43:59 +0200

    made a function 2

--- app/main.py
+++ app/main.py
@@ -9,14 +9,11 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    d = {}
-    m = []
-    for i in range(len(errors)):
-        m.append(format_linter_error(errors[i]))
-    d["errors"] = m
-    d["path"] = file_path
-    d["status"] = " ".join(["failed" if len(m) > 0 else "passed"])
-    return d
+    return {
+        "errors" : [format_linter_error(errors[i]) for i in range(len(errors))],
+        "path" : file_path,
+        "status" : " ".join(["failed" if len([errors]) > 0 else "passed"])
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
