d7a2e4c4bdf865a529e04aac310d8856c311c916
Mykola Moskaliuk <154981650+diexpron@users.noreply.github.com>
2023-12-31 20:59:04 +0200

    fixed 2 function

--- app/main.py
+++ app/main.py
@@ -12,37 +12,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors" : [format_linter_error(errors[i]) for i in range(len(errors))],
         "path" : file_path,
-        "status" : " ".join(["failed" if len([errors]) > 0 else "passed"])
+        "status" : " ".join(["failed" if len(errors) > 0 else "passed"])
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(keys, errors) for keys, errors in linter_report.items()]
 
-report_file = {
-    "./test_source_code_2.py": [],
-    "./source_code_2.py":
-        [
-            {
-                "code": "E501",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 80,
-                "text": "line too long (99 > 79 characters)",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-            {
-                "code": "W292",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 100,
-                "text": "no newline at end of file",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-        ]
-}
 
-print(format_linter_report(report_file))
 
