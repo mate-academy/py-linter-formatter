bf0133fb0df070001c44374ea3e971469d6184d8
Oles Shulzhenko <simplemanua@gmail.com>
2024-01-03 15:38:51 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,48 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            }
+            for error in errors if error
+        ],
+        "path": file_path,
+        "status": "failed"
+    } if errors else {"errors": [], "path": file_path, "status": "passed"}
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
+    return [
+        {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in errors if error
+            ],
+            "path": path,
+            "status": "failed"
+        } if errors else {"errors": [], "path": path, "status": "passed"}
+        for path, errors in linter_report.items()
+    ]
     pass
