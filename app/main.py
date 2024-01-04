895a790965ce9edcce921aa6fe721c22311fb167
Polonnikova Anna <xxxuko@gmail.com>
2024-01-04 16:33:51 +0200

    fixed mistakes

--- app/main.py
+++ app/main.py
@@ -12,12 +12,8 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status":
-            "failed"
-            if [format_linter_error(error) for error in errors]
-            else "passed"
+        "status": "passed" if not errors else "failed"
     }
-
 
 def format_linter_report(linter_report: dict) -> list:
     return [
