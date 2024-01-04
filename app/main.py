6489f42cb87de406a726337e4b38867cfdd50dcd
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-01-04 18:15:40 +0200

    Update app/main.py
    Co-authored-by: Vasyl Hnatiuk <104974881+vasylhnatiuk@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(err) for err in errors],
         "path": file_path,
-        "status": "failed" if any(errors) else "passed"
+        "status": "failed" if errors else "passed"
     }
 
 
