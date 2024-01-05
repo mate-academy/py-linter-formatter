098010531241853470dee2bc5da408cc0575fd04
Nick Illich <nickillich96@gmail.com>
2024-01-05 08:43:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def format_linter_error(error: dict) -> dict:
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
-        "name":  error["code"],
+        "name": error["code"],
         "source": "flake8"
     }
 
@@ -20,4 +20,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
 
-    return [format_single_linter_file(path, errors) for path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
