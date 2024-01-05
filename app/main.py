2ac44a1acf623d945b74d2e5eb5ac0acfce766b6
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-05 18:36:58 +0200

    change return from status

--- app/main.py
+++ app/main.py
@@ -10,12 +10,17 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(item) for item in errors],
+        "errors": [
+            format_linter_error(item)
+            for item in errors
+        ],
         "path": file_path,
-        "status": "passed" if len(errors) == 0 else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key, value
-            in linter_report.items()]
+    return [
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
+    ]
