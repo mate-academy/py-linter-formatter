842ad0e82c7d751c0316c70d417123fdabe09248
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-01-05 13:29:30 +0200

    Solution for task

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"
+        "source": "flake8",
     }
 
 
