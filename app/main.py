bc6aaf023a68cf8ca7b9f1b1bfb8e1dd0b28095d
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-06 10:27:49 +0200

    Update main.py
    fixed flake8 E302 and W391 error

--- app/main.py
+++ app/main.py
@@ -9,12 +9,14 @@ def format_linter_error(error: dict) -> dict[str, str | Any]:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(err) for err in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [
@@ -24,4 +26,3 @@ def format_linter_report(linter_report: dict) -> list:
             "status": format_single_linter_file(path, err)["status"]
         } for path, err in linter_report.items()
     ]
-
