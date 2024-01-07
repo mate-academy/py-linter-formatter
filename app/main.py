048f5673968df18e3da8ffedb1c58066cd94ec62
volry <v.v.ryzhuk@gmail.com>
2024-01-07 09:06:38 +0200

    second function with one line done

--- app/main.py
+++ app/main.py
@@ -9,13 +9,8 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    formatted_errors = [format_linter_error(error) for error in errors]
-    status = "failed" if errors else "passed"
-    return {
-        "errors": formatted_errors,
-        "path": file_path,
-        "status": status
-    }
+    return {"errors": [format_linter_error(error) for error in errors], "path": file_path,
+            "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
