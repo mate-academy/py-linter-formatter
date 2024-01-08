1b9249274f2a15f126ffa5034497a2ff2a9114d4
Serhii Buryk <sergmarten21@gmail.com>
2024-01-08 14:24:37 +0200

    Deleted pass and changed status line

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+
 def format_linter_error(error: dict) -> dict[str, str | Any]:
     return {
         "line": error["line_number"],
@@ -9,6 +10,7 @@ def format_linter_error(error: dict) -> dict[str, str | Any]:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(err) for err in errors],
@@ -16,11 +18,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "failed" if errors else "passed"
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": format_single_linter_file(path, err)["errors"],
-            "path": path,
-            "status": format_single_linter_file(path, err)["status"]
-        } for path, err in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path=path, errors=errors)
+            for path, errors in linter_report.items()]
