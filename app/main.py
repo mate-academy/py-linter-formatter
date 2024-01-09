dbb7d291030ff68636f037b94c56d69497832603
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-09 09:18:32 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": format_single_linter_file(path, error)
-        } for path, error in linter_report.items()
+          format_single_linter_file(path, error)
+        } 
+        for path, error in linter_report.items()
     ]
