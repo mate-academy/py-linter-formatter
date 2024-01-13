5e73eca6ed736617bf9be11fb1aebdfd76c1c0c7
Maksym Hekalo <maximgekalo@gmail.com>
2024-01-13 20:25:31 +0200

    fix variable names and indentations

--- app/main.py
+++ app/main.py
@@ -8,15 +8,9 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-def format_single_linter_file(
-        file_path: str,
-        errors: list
-) -> dict[str, str | list[dict]]:
+def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            format_linter_error(error_log_data)
-            for error_log_data in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if len(errors) != 0 else "passed",
     }
@@ -24,6 +18,6 @@ def format_single_linter_file(
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path_key, list_error_value)
-        for file_path_key, list_error_value in linter_report.items()
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
