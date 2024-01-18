8da58dbe5e65724a4f452d8eafc6997030167979
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-01-18 13:31:55 +0200

    Rename 'file' to 'error'

--- app/main.py
+++ app/main.py
@@ -22,6 +22,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(path, file)
-        for path, file in linter_report.items()
+        format_single_linter_file(path, error)
+        for path, error in linter_report.items()
     ]
