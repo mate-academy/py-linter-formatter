2774ddee5a2df1e9e8390119bb9ca0ef28ab49d3
Misha Goryn <mgoryn68@gmail.com>
2024-01-19 22:12:34 +0200

    "Solution"

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def format_linter_error(error: dict) -> dict:
         "name": error["code"],
         "source": "flake8"
     }
-
     pass
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
