6be6fee52503bf7558711a36c2301959b0135ddd
bogdankoziy <133056142+bogdankoziy@users.noreply.github.com>
2024-01-20 17:55:09 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -13,14 +13,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         {
             "errors": [format_linter_error(error) for error in errors],
             "path": file_path,
-            "status": "failed",
+            "status": "failed" if errors else "passed"
         }
-        if errors != []
-        else {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "passed",
-        }
+
     )
 
 
