7c8e390ac33a706166cc14f4b26aa62a48810725
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2024-01-22 21:54:56 +0200

    Making review's corrections
    - used format_single_linter_file function inside format_linter_report func
    - add a check of a status inside format_single_linter_file func
    
    Signed-off-by: Romanskyy Andrey <andrey.romanskyy@icloud.com>

--- app/main.py
+++ app/main.py
@@ -10,21 +10,15 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(error) for error in errors],
+        "errors": [format_linter_error(error)
+                   for error in errors if len(errors) > 0],
         "path": file_path,
-        "status": "failed",
+        "status": "failed" if len(errors) > 0 else "passed",
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                format_linter_error(error)
-                for error in errors if len(errors) > 0
-            ],
-            "path": file_path,
-            "status": "failed" if len(errors) > 0 else "passed",
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
