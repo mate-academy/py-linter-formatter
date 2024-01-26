a9ee68b28e6968614282f734d7b2089ef03b82f5
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-01-26 21:18:11 +0100

    Solved the second task and corrected the first task

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_name"],
+        "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
         "source": "flake8"
-    }
+        }
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            }
+            for error in errors
+        ],
+        "path": file_path,
+        "status": "failed" if errors != {} else "passed"
+    }
     pass
 
 
