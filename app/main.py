790db4d67246f2a4c80dbf80bb1998d9f6bb6853
Roman Vovchuk <roman11111184@gmail.com>
2024-01-26 15:35:58 +0200

    fix  format_linter_error

--- app/main.py
+++ app/main.py
@@ -13,16 +13,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
         [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+            format_linter_error(error)
             for error in errors
         ],
-            "path": str(file_path),
+            "path": file_path,
             "status": "failed" if errors else "passed"
 
     }
@@ -33,13 +27,7 @@ def format_linter_report(linter_report: dict) -> list:
     return [{
         "errors":
         [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8",
-            }
+            format_linter_error(error)
             for error in file_errors
         ],
         "path": file_path,
