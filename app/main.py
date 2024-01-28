2bcdeafe533e5341c70ab2a47325a45e3327f743
Aleksandr Mazur <a9p9m9@gmail.com>
2024-01-28 16:45:18 +0200

    fix name key and changeing condition operator without using not

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if not errors else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(error, errors)
-            for error, errors in linter_report.items()]
+    return [format_single_linter_file(path, errors)
+            for path, errors in linter_report.items()]
