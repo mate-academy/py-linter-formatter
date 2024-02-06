b78e65e0eb9793f49b2bf19e65ddb9820326af4a
Andrei Butko <andreybutkomerefa@gmail.com>
2024-02-06 15:28:37 +0200

    fix reuse

--- app/main.py
+++ app/main.py
@@ -7,31 +7,16 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
-        ], "path": file_path, "status": "failed"
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
+            "errors": [format_linter_error(error) for error in errors],
             "path": path,
             "status": "failed" if errors else "passed"
         }
