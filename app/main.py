5931af40b116fb263b818715467aa4e799af6581
Maksim Li <sma4no.7@gmail.com>
2024-02-10 18:42:02 +0100

    implemented first and second func

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 def format_linter_error(error: dict) -> dict:
     # write your code here
-    pass
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "source": "flake8"
+            }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
-    pass
-
+    status = "failed" if any(error["code"].startswith("E") for error in errors) else "passed"
+    formatted_errors = [{
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    } for error in errors]
+    return {
+        "errors": formatted_errors,
+        "path": file_path,
+        "status": status
+    }
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
