02a08d178a764a4e3184ecba390c234b8c32ad7c
Maksim Li <sma4no.7@gmail.com>
2024-02-11 13:49:01 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,28 +1,42 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "source": "flake8"
-            }
-
-
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    status = "failed" if any(error["code"].startswith("E") for error in errors) else "passed"
-    formatted_errors = [{
+    return {
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
         "source": "flake8"
-    } for error in errors]
-    return {
-        "errors": formatted_errors,
-        "path": file_path,
-        "status": status
     }
 
+
+def format_single_linter_file(file_path: str, errors: list) -> dict:
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            }
+            for error in errors
+        ],
+        "path": file_path,
+        "status": "failed"
+    }
+
+
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "path": path,
+            "status": "passed" if not errors else "failed",
+            "errors": [{
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            } for error in errors]
+        } if errors else {"path": path, "status": "passed", "errors": []}
+        for path, errors in linter_report.items()
+    ]
