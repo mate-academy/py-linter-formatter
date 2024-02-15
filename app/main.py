49a243561d182f2251bd5ef88fa264f56359fb09
Yevhen Mykytsei <mykytcey@gmail.com>
2024-02-15 02:38:49 +0100

    Solution, its one of the most difficult task

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(error) for error in errors], "path": file_path,
-            "status": "passed" if not errors else "failed"}
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path, "status": "passed"
+            if not errors else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [format_linter_error(error) for error in errors], "path": file_path,
-            "status": "passed" if not errors else "failed"}
+    return [{"errors": [format_linter_error(error) for error in errors],
+             "path": file_path, "status": "passed"
+            if not errors else "failed"}
             for file_path, errors in linter_report.items()]
