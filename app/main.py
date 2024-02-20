840bdea30e5f2e74441b1d712b70862815fcf20e
unknown <ruslan.b201520@gmail.com>
2024-02-20 19:00:06 +0200

    code update

--- app/main.py
+++ app/main.py
@@ -9,37 +9,17 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    errors_list = []
-    status = "failed" if len(errors) != 0 else "passed"
-    if status == "failed":
-        for error in errors:
-            formatted_error = format_linter_error(error)
-            errors_list.append(formatted_error)
-    result_dict = {"errors": errors_list,
-                   "path": file_path,
-                   "status": status}
-    return result_dict
+    return {"errors": [format_linter_error(error) for error in errors] if errors else [], 
+            "path": file_path, 
+            "status": "passed" if not errors else "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    result = []
-    file_names = list(linter_report.keys())
-    status_list = []
-    for file_name in file_names:
-        status_list.append("passed" if len(linter_report[file_name]) == 0
-                           else "failed")
-    for value in range(len(status_list)):
-        sub_list = []
-        if status_list[value] == "failed":
-            errors_list = linter_report[file_names[value]]
-            for error in errors_list:
-                formatted_error = format_linter_error(error)
-                sub_list.append(formatted_error)
-
-        sub_dict = {"errors": sub_list,
-                    "path": file_names[value],
-                    "status": status_list[value]}
-
-        result.append(sub_dict)
-
-    return result
+    return [
+        {
+            "errors": [format_linter_error(error) for error in linter_report[file_name]],
+            "path": file_name,
+            "status": "failed" if linter_report[file_name] else "passed"
+        }
+        for file_name in linter_report.keys()
+    ]
