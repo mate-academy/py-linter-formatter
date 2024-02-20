a9f2c6c8b05564be0225e9c71594e31bbf87c7d5
unknown <ruslan.b201520@gmail.com>
2024-02-20 19:06:03 +0200

    code update

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error) for error in errors]
-                      if errors else [], 
+            if errors else [],
             "path": file_path,
             "status": "passed" if not errors else "failed"}
 
@@ -19,7 +19,7 @@ def format_linter_report(linter_report: dict) -> list:
     return [
         {
             "errors": [format_linter_error(error) 
-                       for error in linter_report[file_name]],
+            for error in linter_report[file_name]],
             "path": file_name,
             "status": "failed" if linter_report[file_name] else "passed"
         }
