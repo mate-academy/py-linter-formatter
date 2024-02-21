395628227645c775251a959207fac9d301d1a3a8
Olha <olgazinevych@gmail.com>
2024-02-21 20:48:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        key: (
+            error["line_number"] if key == "line" else
+            error["column_number"] if key == "column" else
+            error["text"] if key == "message" else
+            error["code"] if key == "name" else
+            "flake8"
+        )
+        for key in ["line", "column", "message", "name", "source"]
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        key: (
+            [format_linter_error(error) for error in errors]
+            if key == "errors" else
+            file_path if key == "path" else
+            "failed" if errors else
+            "passed"
+        )
+        for key in ["errors", "path", "status"]
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(path, error)
+        for path, error in linter_report.items()
+    ]
