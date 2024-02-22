21833304dae18630721dc33c7995080d356df780
Klishch Nazar <klishch65@gmail.com>
2024-02-22 22:36:53 +0200

    Solution with reuse functions inside another functions

--- app/main.py
+++ app/main.py
@@ -13,17 +13,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                {
-                    "line": error["line_number"]
-                    if "line_number" in error else None,
-                    "column": error["column_number"]
-                    if "column_number" in error else None,
-                    "message": error["text"]
-                    if "text" in error else None,
-                    "name": error["code"]
-                    if "code" in error else None,
-                    "source": "flake8"
-                }
+                format_linter_error(error)
                 for error in errors
             ],
         "path": file_path,
@@ -36,29 +26,11 @@ def format_linter_report(linter_report: dict) -> list:
     return [
         {
             "errors": [],
-            "path": path,
+            "path": file_path,
             "status": "passed"
         }
         if errors == []
-        else {
-            "errors":
-            [
-                {
-                    "line": error["line_number"]
-                    if "line_number" in error else None,
-                    "column": error["column_number"]
-                    if "column_number" in error else None,
-                    "message": error["text"]
-                    if "text" in error else None,
-                    "name": error["code"]
-                    if "code" in error else None,
-                    "source": "flake8"
-                }
-                for error in errors
-            ],
-            "path": path,
-            "status": "failed"
-        }
-        for path, errors in linter_report.items()
+        else format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
     pass
