79af10d8d811ca8912c54caafb97dbbf00e0d3e7
Oleksandr <oleksandrkachura9@gmail.com>
2024-02-22 15:14:55 +0200

    Use format_linter_error to avoid code duplication

--- app/main.py
+++ app/main.py
@@ -1,24 +1,15 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"], "column": error["column_number"],
-            "message": error["text"], "name": error["code"],
+    return {"line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
             "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
-    return {"errors": [{"line": item["line_number"],
-                        "column": item["column_number"],
-                        "message": item["text"], "name": item["code"],
-                        "source": "flake8"}
-                       for item in errors], "path": file_path,
-            "status": "failed"}
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path, "status": "failed" if errors else "passed"}
 
 
-def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [{"line": error["line_number"],
-                         "column": error["column_number"],
-                         "message": error["text"], "name": error["code"],
-                         "source": "flake8"}
-                        for error in errors], "path": path,
-             "status": "failed" if errors else "passed"}
-            for path, errors in linter_report.items()]
+# def format_linter_report(linter_report: dict) -> list:
+#     return []
