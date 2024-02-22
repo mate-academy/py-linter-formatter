09eaff569d1440266c1638ff5cc60b54b867c80c
Vova <v.kampanchenko@gmail.com>
2024-02-23 01:23:07 +0200

    add main code

--- app/main.py
+++ app/main.py
@@ -1,13 +1,51 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
+
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            } for error in errors
+        ],
+        "path": "./source_code_2.py",
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+
+    return [
+        {
+            "errors": [],
+            "path": "./test_source_code_2.py",
+            "status": "passed"
+        },
+        {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                } for error in errors
+            ],
+            "path": "./source_code_2.py",
+            "status": "failed"
+        } for errors in linter_report["./source_code_2.py"]
+    ]
