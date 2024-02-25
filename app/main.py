afe0d504587afa90a3e9cef05728e68138006073
Klishch Nazar <klishch65@gmail.com>
2024-02-25 12:45:37 +0200

    Fixed functions

--- app/main.py
+++ app/main.py
@@ -11,26 +11,17 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":
-            [
-                format_linter_error(error)
-                for error in errors
-            ],
+        "errors": [] if not errors
+        else [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors != 0 else "passed"
+        "status": "passed" if not errors else "failed"
     }
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [],
-            "path": file_path,
-            "status": "passed"
-        }
-        if errors == []
-        else format_single_linter_file(file_path, errors)
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
     pass
