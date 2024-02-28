8295fba7de32abaebf07cceead69e3693978efd1
Olena Poplias <poplyasitasony@gmail.com>
2024-02-28 12:06:07 -0500

    "{,[ placed on a new line. Changed variable names"

--- app/main.py
+++ app/main.py
@@ -4,17 +4,20 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8", }
+        "source": "flake8",
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if errors else "passed", }
+        "status": "failed" if errors else "passed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(key, value)
-        for key, value in linter_report.items()]
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
