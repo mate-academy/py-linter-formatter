5f519cd69f54f5cb5491f3e27c9b56dc520d7428
Valentin Pozniak <waliker448@gmail.com>
2024-03-09 21:23:42 +0200

    'fix_3_deleted_newlines'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +9,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {"errors": [
         format_linter_error(error)
         for error in errors
@@ -21,7 +19,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-
     return [
         format_single_linter_file(path, error)
         for path, error in linter_report.items()
