e89e8085ed71430b3e9146d87422f68339603a83
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-03-11 08:26:50 -0700

    Solution to linter formater

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    
+    return {
+        change_name_dict[key]: (error[key] if key in error.keys() else key) 
+        for key in change_name_dict.keys()
+    }
 
+change_name_dict = {
+    "line_number": "line",
+    "column_number": "column",
+    "text": "message",
+    "code": "name",
+    "flake8": "source"
+}
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    
+    return {"errors": [], "path": file_path, "status": "passed"} if errors == [] else {"errors": [format_linter_error(error) for error in errors],
+    "path": file_path, "status": "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    
+    return list(
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    )
+        
+
