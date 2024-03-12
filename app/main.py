70b479eae56bd3a3976943665790724acc288b60
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-03-11 19:56:55 -0700

    Rewrote format_linter_error and format_single_linter_file

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        change_name_dict[key]: (error[key] if key in error.keys() else key)
-        for key in change_name_dict.keys()
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
     }
 
 
-change_name_dict = {"line_number": "line", "column_number": "column",
-                    "text": "message", "code": "name", "flake8": "source"}
-
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [], "path": file_path, "status": "passed"} \
-        if errors == [] else {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": file_path, "status": "failed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
