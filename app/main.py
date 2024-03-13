8133fcd5217ae3174554bc736c80c0c60e981baf
Artem Andriushchenko <link.by.artem@gmail.com>
2024-03-13 03:26:19 +0200

    Called format_linter_error instead of duplicating code

--- app/main.py
+++ app/main.py
@@ -10,16 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error.get("line_number"),
-                "column": error.get("column_number"),
-                "message": error.get("text"),
-                "name": error.get("code"),
-                "source": "flake8"
-            }
-            for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
