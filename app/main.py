8b40280bf4ff13e0484cf40ea0653770adceb7df
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-03-13 21:06:12 +0100

    Solution for flake8 reports

--- app/main.py
+++ app/main.py
@@ -1,13 +1,40 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "column": error["column_number"],
+        "line": error["line_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            {
+                "column": error["column_number"],
+                "line": error["line_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8",
+            }
+            for error in errors
+
+        ],
+        "path": file_path,
+        "status": "failed" if len(errors) else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [{
+        "errors": [{
+            "column": error["column_number"],
+            "line": error["line_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8",
+        } for error in errors],
+        "path": path,
+        "status": "failed" if len(errors) else "passed"
+    } for path, errors in linter_report.items()]
