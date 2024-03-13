03afda02e5d82b0d2c18fc3c3f880187ebbd6c74
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-03-13 19:33:05 +0200

    Placed all keys on the same intendetional with error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def format_linter_error(error: dict) -> dict:
+
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -9,14 +10,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+
     return {
         "errors":
             [
                 format_linter_error(error)
                 for error in errors
             ],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
 
 
