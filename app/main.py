37d6b6d09b3df489aecf5eeb7cc57589342add64
bwaah1 <chykivan@gmail.com>
2024-03-14 13:11:53 +0200

    Fix format_linter_report function

--- app/main.py
+++ app/main.py
@@ -15,18 +15,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
                 format_linter_error(error) for error in errors
             ],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(path, errors)
-        if errors
-        else {
-            "errors": [],
-            "path": path,
-            "status": "passed"
-        }
         for path, errors in linter_report.items()
     ]
