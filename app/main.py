11ad9cb189846eb91a9b416cc402785309dd62d6
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-03-16 22:29:15 +0100

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-            format_single_linter_file(reports, report) for reports in linter_report
-            for report in linter_report[reports]
+        format_single_linter_file(reports, report)
+        for reports in linter_report
+        for report in linter_report[reports]
     ]
