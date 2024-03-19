a531d03b1dfbba82052498735219189d65145fd8
Volkov <volkovyegorandriiovich@gmail.com>
2024-03-19 14:40:08 +0200

    correctives applied

--- app/main.py
+++ app/main.py
@@ -10,43 +10,14 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": errors[i]["line_number"],
-                "column": errors[i]["column_number"],
-                "message": errors[i]["text"],
-                "name": errors[i]["code"],
-                "source": "flake8"
-            }
-            for i in range(len(errors))
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors != [] else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [],
-            "path": key,
-            "status": "passed"
-        }
-        if linter_report[key] == []
-        else
-        {
-            "errors": [
-                {
-                    "line": linter_report[key][i]["line_number"],
-                    "column": linter_report[key][i]["column_number"],
-                    "message": linter_report[key][i]["text"],
-                    "name": linter_report[key][i]["code"],
-                    "source": "flake8"
-                }
-                for i in range(len(linter_report[key]))
-            ],
-            "path": key,
-            "status": "failed"
-        }
+        format_single_linter_file(key, linter_report[key])
         for key in linter_report
     ]
