a4be6b147df2397c01547ab2bfac14ac0cc6ea92
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-03-19 06:50:28 +0000

    changed i to more descriptive

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(i) for i in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
@@ -18,5 +18,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(i, linter_report[i]) for i in linter_report
+        format_single_linter_file(report, linter_report[report]) for report in linter_report
     ]
