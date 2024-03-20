5f2c2ea7f2ba8920d5ff6892d102d9f41b3270c2
Andriy <grushevoyandrey@gmail.com>
2024-03-20 22:04:30 +0300

    fix ыsingle_linter and format_linter

--- app/main.py
+++ app/main.py
@@ -7,10 +7,12 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(index) for index in errors],
-        "path": file_path, "status": "failed" if errors else "passed"}
+        "path": file_path,
+        "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(path, errors)
-        for path, errors in linter_report.items()]
+        for path, errors in linter_report.items()
+    ]
