60543f88e1c27f08372813141dfab1b7d9c93cfc
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-03-23 00:44:25 +0200

    implemented format_linter_error method

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    name = error.get("code", "")
+    formatted_error = {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": name,
+        "source": "flake8"
+    }
+    return formatted_error
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
