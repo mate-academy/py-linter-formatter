edef09d922c0da5fb3320a54d1010e0e0f407da0
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2024-03-23 01:09:08 +0200

    implemented format_single_linter_file method

--- app/main.py
+++ app/main.py
@@ -11,8 +11,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    formatted_errors = [format_linter_error(error) for error in errors]
+    status = "failed" if formatted_errors else "passed"
+
+    formatted_error = {
+        "errors": formatted_errors,
+        "path": file_path,
+        "status": status
+    }
+    return formatted_error
 
 
 def format_linter_report(linter_report: dict) -> list:
