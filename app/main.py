7c1c78f8291b9599b307aadc4cdd3b09672173c4
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-03-24 15:33:08 +0100

    Linter formatter task completed

--- app/main.py
+++ app/main.py
@@ -24,17 +24,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                {
-                    "line": error.get("line_number"),
-                    "column": error.get("column_number"),
-                    "message": error.get("text"),
-                    "name": error.get("code"),
-                    "source": "flake8"
-                } for error in linter_report[file_path]
-            ],
-            "path": file_path,
-            "status": "passed" if not linter_report[file_path] else "failed"
-        } for file_path in linter_report
+        format_single_linter_file(file_path, linter_report[file_path])
+        for file_path in linter_report
     ]
