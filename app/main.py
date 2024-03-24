5c16d882c258d88a729e6189fcdd733eba7580e0
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-03-24 17:56:44 +0100

    Linter formatter task completed

--- app/main.py
+++ app/main.py
@@ -16,10 +16,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "passed" if not errors else "failed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
