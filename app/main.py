675dba81933eeae33cf79cbfc59cd13f0aeab545
Oleksandr Taranenko <oleksandrtaranenko239@gmail.com>
2024-03-27 20:45:42 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -18,15 +18,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [],
-            "path": file_path,
-            "status": "passed"
-        }
-        if not errors else {
-            "errors": [format_linter_error(error) for error in errors],
-            "path": file_path,
-            "status": "failed"
-        }
+        format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
